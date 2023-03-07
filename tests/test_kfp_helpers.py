6cd29eaf9f50ebb2eb294494e229f58b754ff33c
Philippe Moussalli <philippe@moussalli@ml6.eu>
2023-03-07 17:08:53 +0100

    add kfp helpers test

---
+++ tests/test_kfp_helpers.py
@@ -0,0 +1,19 @@
+"""
+Test scripts for kfp helpers
+"""
+import pytest
+
+from express_components.helpers.kfp_helpers import parse_kfp_list
+
+
+@pytest.mark.parametrize(
+    "kfp_parsed_string, expected_output",
+    [
+        ("['item1', 'item2', 'item3']", ['item1', 'item2', 'item3']),
+        ("[]", []),
+        ("['']", ['']),
+        ("['1', '2', '3']", ['1', '2', '3']),
+    ],
+)
+def test_parse_kfp_list(kfp_parsed_string, expected_output):
+    assert parse_kfp_list(kfp_parsed_string) == expected_output
