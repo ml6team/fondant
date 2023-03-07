ad0c70f9f2c5380374890a7b5295cdf6a4abd45c
Philippe Moussalli <philippe@moussalli@ml6.eu>
2023-03-07 17:39:35 +0100

    add logger tests

--- mlpipelines/common/src/express_components/helpers/logger.py
+++ mlpipelines/common/src/express_components/helpers/logger.py
@@ -7,12 +7,12 @@ import os
 LOG_LEVEL = os.environ.get("LOG_LEVEL", default="INFO")
 
 
-def configure_logging() -> None:
+def configure_logging(log_level=LOG_LEVEL) -> None:
     """Configure the root logger based on config settings"""
     logger = logging.getLogger()
 
     # set loglevel
-    level = logging.getLevelName(LOG_LEVEL)
+    level = logging.getLevelName(log_level)
     logger.setLevel(level)
 
     # logging stdout handler
