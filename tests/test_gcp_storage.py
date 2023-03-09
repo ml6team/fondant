6e4002f570bd987f47b8ebf452f23de423c48d22
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-09 09:26:42 +0100

    Add express tests for helper functionalities
    This PR adds basic pytest for the helper functionalities provided in
    Express:
    
    - Logger
    - Storage
    - kfp Utils
    - io
    - manifest
    
    Next steps is to include tests for the Express pandas components + run
    tests with github actions
    
    ---------
    
    Co-authored-by: Philippe Moussalli <philippe@moussalli@ml6.eu>

---
+++ tests/test_gcp_storage.py
@@ -0,0 +1,24 @@
+"""
+Test scripts for gcp storage functionalities
+"""
+import pytest
+
+from express.storage_interface import DecodedBlobPath
+from express.gcp_storage import StorageHandler
+
+
+@pytest.mark.parametrize("fully_qualified_blob_path, expected_result", [
+
+    ("gs://my-bucket/my-file.txt",
+     DecodedBlobPath(bucket_name="my-bucket", blob_path="my-file.txt")),
+
+    ("gs://my-bucket/my-folder/my-file.txt",
+     DecodedBlobPath(bucket_name="my-bucket", blob_path="my-folder/my-file.txt")),
+
+    ("gs://bucket/path/to/my-file.test.txt",
+     DecodedBlobPath(bucket_name="bucket", blob_path="path/to/my-file.test.txt"))
+])
+def test_decode_blob_path(fully_qualified_blob_path, expected_result):
+    """Test blob path decoding"""
+    handler = StorageHandler()
+    assert handler.decode_blob_path(fully_qualified_blob_path) == expected_result
