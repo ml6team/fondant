0b19d1e157bb3cb03dda66005525ebe6bb04f1e6
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-16 18:33:11 +0100

    add bash script to build images

---
+++ examples/pipelines/finetune_stable_diffusion/build_images.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source ../components.config
+
+IMAGE_TAG="latest"
+
+# Get the flag, if it was passed
+if [[ "$1" == "--build-dir" ]]; then
+  build_dir=true
+else
+  build_dir=false
+fi
+
+# Get the component directory
+component_dir=$(pwd)/"components"
+
+# Loop through all subdirectories
+for dir in $component_dir/*/; do
+  cd "$dir"
+  basename=${dir%/}
+  basename=${basename##*/}
+  echo $basename
+  # Build all images or one image depending on the passed argument
+  if [[ $build_dir == true && "$basename" == "$2/" ]] || [[ $build_dir == false ]]; then
+    image_name="components/${PWD##*/}"
+    full_image_name=${PROJECT_ARTIFACT_PATH}/${image_name}:${IMAGE_TAG}
+    echo $full_image_name
+    gcloud builds submit --machine-type n1-highcpu-32 . -t "$full_image_name"
+  fi
+  cd "$component_dir"
+done
