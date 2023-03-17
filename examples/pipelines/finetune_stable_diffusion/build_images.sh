0761a8549aba55d1def5e5bc6497dc7dbfa63aea
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-17 13:25:01 +0100

    change docker registry build to public repository

--- examples/pipelines/finetune_stable_diffusion/build_images.sh
+++ examples/pipelines/finetune_stable_diffusion/build_images.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source ../components.config
-
-IMAGE_TAG="latest"
+source ../docker_build.config
 
 # Get the flag, if it was passed
 if [[ "$1" == "--build-dir" ]]; then
@@ -17,15 +15,14 @@ component_dir=$(pwd)/"components"
 # Loop through all subdirectories
 for dir in $component_dir/*/; do
   cd "$dir"
-  basename=${dir%/}
-  basename=${basename##*/}
+  BASENAME=${dir%/}
+  BASENAME=${BASENAME##*/}
   # Build all images or one image depending on the passed argument
-  if [[ $build_dir == true && "$basename" == "$2" ]] || [[ $build_dir == false ]]; then
-    image_name="components/${PWD##*/}"
-    full_image_name=${PROJECT_ARTIFACT_PATH}/${image_name}:${IMAGE_TAG}
+  if [[ $build_dir == true && "$BASENAME" == "$2" ]] || [[ $build_dir == false ]]; then
+    full_image_name=${DOCKER_NAMESPACE}/${BASENAME}:${IMAGE_TAG}
     echo $full_image_name
-    #TODO: replace with docker build after deciding on container locations
-    gcloud builds submit --machine-type n1-highcpu-32 . -t "$full_image_name"
+    docker build . -t "$full_image_name"
+    docker push "$full_image_name"
   fi
   cd "$component_dir"
 done
