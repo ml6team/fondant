276e91b321d2f17bc5bb6eaab4e3bcaefd355d27
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-20 17:53:58 +0100

    add function to log cuda devices

--- express/utils.py
+++ express/utils.py
@@ -6,6 +6,8 @@ import importlib.util
 import importlib.metadata
 from typing import Sequence, Union, List
 
+import pynvml
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,3 +30,24 @@ def is_module_available(module_name: Union[str, Sequence[str]]) -> List[str]:
             module_version = importlib.metadata.version(module)
             logger.info(f"{module} version {module_version} available.")
     return missing_modules
+
+
+def get_cuda_availability():
+    """Function that checks if a cuda device is available"""
+
+    def _round_bytes_to_gb(byte_size):
+        return round(byte_size / 1024**3, 1)
+
+    try:
+        logger.info("Driver Version: %s", pynvml.nvmlSystemGetDriverVersion())
+        device_cnt = pynvml.nvmlDeviceGetCount()
+        logger.info("Found %s cuda device(s)", device_cnt)
+        for device_idx in range(device_cnt):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(device_idx)
+            info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+            logger.info("Device: %s", pynvml.nvmlDeviceGetName(handle))
+            logger.info("Total memory: %s GB", _round_bytes_to_gb(info.total))
+            logger.info("Free memory: %s GB", _round_bytes_to_gb(info.free))
+            logger.info("Used memory: %s GB", _round_bytes_to_gb(info.used))
+    except pynvml.NVMLError:
+        logger.warning("Cuda device(s) not found.")
