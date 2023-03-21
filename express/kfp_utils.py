f3737f501455224b839ab0839958559a0789f299
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-21 10:24:07 +0100

    Refactor pipelines directory (#15)
    PR to refactor pipelines directory:
    
    - Pulling out common configs to root directory
    - moving kfp compiler to common express helpers
    - resolving some pip issues with kfp and adding kfp as a dependency
    - cleaning up some files
    - Bugfix kfp upload

--- express/kfp_utils.py
+++ express/kfp_utils.py
@@ -1,6 +1,15 @@
 """General kfp helpers"""
+import os
 import ast
 import logging
+from typing import Callable
+
+import kfp
+from kfp import compiler
+
+from express.logger import configure_logging
+
+configure_logging()
 
 logger = logging.getLogger(__name__)
 
@@ -18,3 +27,33 @@ def parse_kfp_list(kfp_parsed_string: str) -> list:
         list: the list representation of the json string
     """
     return ast.literal_eval("".join(kfp_parsed_string))
+
+
+def compile_and_upload_pipeline(
+    pipeline: Callable[[], None], host: str, env: str
+) -> None:
+    """Upload pipeline to kubeflow.
+    Args:
+        pipeline (Callable): function that contains the pipeline definition
+        host (str): the url host for kfp
+        env (str): the project run environment (sbx, dev, prd)
+    """
+    client = kfp.Client(host=host)
+
+    pipeline_name = f"{pipeline.__name__}:{env}"
+    pipeline_filename = f"{pipeline_name}.yaml"
+    compiler.Compiler().compile(pipeline_func=pipeline, package_path=pipeline_filename)
+
+    existing_pipelines = client.list_pipelines(page_size=100).pipelines
+    for existing_pipeline in existing_pipelines:
+        if existing_pipeline.name == pipeline_name:
+            # Delete existing pipeline before uploading
+            logger.warning(
+                f"Pipeline {pipeline_name} already exists. Deleting old pipeline..."
+            )
+            client.delete_pipeline_version(existing_pipeline.default_version.id)
+            client.delete_pipeline(existing_pipeline.id)
+
+    logger.info(f"Uploading pipeline: {pipeline_name}")
+    client.upload_pipeline(pipeline_filename, pipeline_name=pipeline_name)
+    os.remove(pipeline_filename)
