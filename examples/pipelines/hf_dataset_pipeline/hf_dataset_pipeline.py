f943cb73c2ecf7c1814eb0f2398c8f6d23062096
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-03-21 10:53:27 +0100

    Use relative paths

--- examples/pipelines/hf_dataset_pipeline/hf_dataset_pipeline.py
+++ examples/pipelines/hf_dataset_pipeline/hf_dataset_pipeline.py
@@ -16,16 +16,14 @@ run_id = '{{workflow.name}}'
 artifact_bucket = KubeflowConfig.ARTIFACT_BUCKET
 
 # Component 1: load from hub
-load_from_hub_op = comp.load_component(
-    '/Users/nielsrogge/Documents/python_projects/express/examples/pipelines/hf_dataset_pipeline/components/load_from_hub/component.yaml')
+load_from_hub_op = comp.load_component('components/load_from_hub/component.yaml')
 load_from_hub_extra_args = {"dataset_name": GeneralConfig.DATASET_NAME}
 load_from_hub_metadata_args = {"run_id": run_id, "component_name": load_from_hub_op.__name__, "artifact_bucket": artifact_bucket}
 load_from_hub_extra_args = json.dumps(load_from_hub_extra_args)
 load_from_hub_metadata_args = json.dumps(load_from_hub_metadata_args)
 
 # Component 2: add captions
-add_captions_op = comp.load_component(
-    '/Users/nielsrogge/Documents/python_projects/express/examples/pipelines/hf_dataset_pipeline/components/add_captions/component.yaml')
+add_captions_op = comp.load_component('components/add_captions/component.yaml')
 add_captions_extra_args = {}
 add_captions_metadata_args = {"run_id": run_id, "component_name": add_captions_op.__name__, "artifact_bucket": artifact_bucket}
 add_captions_extra_args = json.dumps(add_captions_extra_args)
