ffa4c6c2f3c497ecff2dc2e2f59e59094df97239
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-03-22 17:53:17 +0100

    switch over public regsitry from docker to github

--- examples/pipelines/finetune_stable_diffusion/build_images.sh
+++ examples/pipelines/finetune_stable_diffusion/build_images.sh
@@ -19,12 +19,13 @@ for dir in $component_dir/*/; do
   BASENAME=${BASENAME##*/}
   # Build all images or one image depending on the passed argument
   if [[ $build_dir == true && "$BASENAME" == "$2" ]] || [[ $build_dir == false ]]; then
-    full_image_name=${DOCKER_NAMESPACE}/${BASENAME}:${IMAGE_TAG}
+    full_image_name=ghcr.io/${NAMESPACE}/${BASENAME}:${IMAGE_TAG}
     echo $full_image_name
     docker build -t "$full_image_name" \
      --build-arg COMMIT_SHA=$(git rev-parse HEAD) \
      --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
      --build-arg BUILD_TIMESTAMP=$(date '+%F_%H:%M:%S') \
+     --label org.opencontainers.image.source=https://github.com/${NAMESPACE}/${REPO_NAME} \
      .
     docker push "$full_image_name"
   fi
