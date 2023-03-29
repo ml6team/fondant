74740f38f5d7863688515a1fc160334b2cd16cc8
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-03-29 11:48:11 +0200

    Address comments

--- examples/pipelines/finetune_stable_diffusion/components/dataset_loader_component/src/main.py
+++ examples/pipelines/finetune_stable_diffusion/components/dataset_loader_component/src/main.py
@@ -6,7 +6,10 @@ from typing import Optional, Union, Dict
 
 from datasets import Dataset, load_dataset
 
-from express.components.hf_datasets_components import HFDatasetsLoaderComponent, HFDatasetsDatasetDraft
+from express.components.hf_datasets_components import (
+    HFDatasetsLoaderComponent,
+    HFDatasetsDatasetDraft,
+)
 from express.logger import configure_logging
 
 configure_logging()
@@ -15,21 +18,20 @@ logger = logging.getLogger(__name__)
 
 def create_metadata(examples):
     images = examples["image"]
-    sizes = [image.size for image in images]
-    
+
     batch = {}
-    batch["width"] = [size[0] for size in sizes]
-    batch["height"] = [size[1] for size in sizes]
-    
+    batch["width"], batch["height"] = zip(*[image.size for image in images])
+
     return batch
 
 
 class DatasetLoaderComponent(HFDatasetsLoaderComponent):
-    """Class that inherits from Hugging Face data loading """
+    """Class that inherits from Hugging Face data loading"""
 
     @classmethod
-    def load(cls, extra_args: Optional[
-        Dict[str, Union[str, int, float, bool]]] = None) -> HFDatasetsDatasetDraft:
+    def load(
+        cls, extra_args: Optional[Dict[str, Union[str, int, float, bool]]] = None
+    ) -> HFDatasetsDatasetDraft:
         """
         An example function showcasing the data loader component using Express functionalities
         Args:
@@ -46,21 +48,31 @@ class DatasetLoaderComponent(HFDatasetsLoaderComponent):
         # 2) Create an example index
         logger.info("Creating index...")
         index_list = [f"image_{idx}" for idx in range(len(dataset))]
-        
+
         # 3) Create dataset draft (manifest without metadata)
         # We store the index itself also as a HF Dataset
         logger.info("Creating draft...")
         index_dataset = Dataset.from_dict({"index": index_list})
-        image_dataset = dataset.remove_columns(["text"]).add_column(name="index", column=index_list)
-        text_dataset = dataset.remove_columns(["image"]).add_column(name="index", column=index_list)
-        metadata_dataset = image_dataset.map(create_metadata, batched=True, remove_columns=["image"])
-        data_sources = {"images": image_dataset,
-                        "captions": text_dataset,
-                        "image_metadata": metadata_dataset}
-        dataset_draft = HFDatasetsDatasetDraft(index=index_dataset, data_sources=data_sources)
+        image_dataset = dataset.remove_columns(["text"]).add_column(
+            name="index", column=index_list
+        )
+        text_dataset = dataset.remove_columns(["image"]).add_column(
+            name="index", column=index_list
+        )
+        metadata_dataset = image_dataset.map(
+            create_metadata, batched=True, remove_columns=["image"]
+        )
+        data_sources = {
+            "images": image_dataset,
+            "captions": text_dataset,
+            "image_metadata": metadata_dataset,
+        }
+        dataset_draft = HFDatasetsDatasetDraft(
+            index=index_dataset, data_sources=data_sources
+        )
 
         return dataset_draft
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     DatasetLoaderComponent.run()
