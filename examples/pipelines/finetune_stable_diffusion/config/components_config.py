70ed04b63614ead70218052385fff0988d9ede2d
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-03-30 14:26:30 +0200

    Update dockerfile

--- examples/pipelines/finetune_stable_diffusion/config/components_config.py
+++ examples/pipelines/finetune_stable_diffusion/config/components_config.py
@@ -38,3 +38,25 @@ class EmbeddingConfig:
 
     MODEL_ID = "openai/clip-vit-large-patch14"
     BATCH_SIZE = 10
+
+
+@dataclass
+class ClipRetrievalConfig:
+    """
+    Configs for CLIP image retrieval component
+    Params:
+        LAION_INDEX_URL(str): url of the indices of the metadata. Those indices need to be
+         transformed in case you decide to use only a subset of the dataset
+        LAION_METADATA_URL (str): url to the metadata of laion dataset metadata (arrow format). It
+         can either contain a subset of the laion 5b metadata (e.g. laion-en) or all of the metadata
+        NB_IMAGES_KNN (int): The ratio of number of image to retrieve via the knn strategy
+         (per image)
+        NB_IMAGES_CENTROID (int): The ratio of number of image to retrieve via the centroid strategy
+    """
+
+    LAION_INDEX_URL = "gs://express-sd-datasets/laion-5b/2b-en/image.index/*"
+    LAION_METADATA_URL = (
+        "gs://express-sd-datasets/laion-5b/metadata/metadata/2B-en.arrow"
+    )
+    NB_IMAGES_KNN = 500
+    NB_IMAGES_CENTROID = 1_000_000
