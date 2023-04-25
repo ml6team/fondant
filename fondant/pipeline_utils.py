794ad2fc05f284e8cfae3d1654c89fbc983e9f4c
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-04-25 14:35:42 +0200

    add hotfix for uploading kubeflow pipelines

--- fondant/pipeline_utils.py
+++ fondant/pipeline_utils.py
@@ -2,24 +2,27 @@
 
 import os
 import logging
-from typing import Callable
+from typing import Callable, Optional
 
 from fondant.import_utils import is_kfp_available
 
 if is_kfp_available():
     import kfp
+    from kfp_server_api.exceptions import ApiException
 
 logger = logging.getLogger(__name__)
 
 
 def compile_and_upload_pipeline(
-    pipeline: Callable[[], None], host: str, env: str
+        pipeline: Callable[[], None], host: str, env: str, pipeline_id: Optional[str] = None
 ) -> None:
     """Upload pipeline to kubeflow.
     Args:
-        pipeline (Callable): function that contains the pipeline definition
-        host (str): the url host for kfp
-        env (str): the project run environment (sbx, dev, prd)
+        pipeline: function that contains the pipeline definition
+        host: the url host for kfp
+        env: the project run environment (sbx, dev, prd)
+        pipeline_id: pipeline id of existing component under the same name. Pass it on when you
+        want to delete current existing pipelines
     """
     client = kfp.Client(host=host)
 
@@ -29,16 +32,22 @@ def compile_and_upload_pipeline(
         pipeline_func=pipeline, package_path=pipeline_filename
     )
 
-    existing_pipelines = client.list_pipelines(page_size=100).pipelines
-    for existing_pipeline in existing_pipelines:
-        if existing_pipeline.name == pipeline_name:
-            # Delete existing pipeline before uploading
-            logger.warning(
-                f"Pipeline {pipeline_name} already exists. Deleting old pipeline..."
-            )
-            client.delete_pipeline_version(existing_pipeline.default_version.id)
-            client.delete_pipeline(existing_pipeline.id)
+    if pipeline_id is not None:
+        pipeline_versions = client.list_pipeline_versions(pipeline_id).versions
+        if pipeline_versions is not None:
+            versions_ids = [getattr(version, "id") for version in pipeline_versions]
+            for version_id in versions_ids:
+                client.delete_pipeline_version(version_id)
+            logger.info("Found existing pipeline under the same. Deleting the pipeline")
+        client.delete_pipeline(pipeline_id)
 
     logger.info(f"Uploading pipeline: {pipeline_name}")
-    client.upload_pipeline(pipeline_filename, pipeline_name=pipeline_name)
+
+    try:
+        client.upload_pipeline(pipeline_filename, pipeline_name=pipeline_name)
+    except Exception as e:
+        raise ApiException(
+            f"Failed to upload the pipeline. Make sure that the pipeline {pipeline_name} does"
+            f" not exist. If you have a pipeline under a similar name, pass in the `pipeline id`"
+            f" in order to delete the existing pipeline") from e
     os.remove(pipeline_filename)
