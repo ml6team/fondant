fc6c51c84f717a82abaa7ad4ba63604f2bd6b6d0
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-03 17:03:54 +0200

    Fix tests

--- tests/test_manifest_evolution.py
+++ tests/test_manifest_evolution.py
@@ -28,6 +28,6 @@ def examples():
 def test_evolution(input_manifest, component_spec, output_manifest):
     manifest = Manifest(input_manifest)
     component_spec = FondantComponentSpec(component_spec)
-    evolved_manifest = manifest.evolve(component_id="100", component_spec=component_spec)
+    evolved_manifest = manifest.evolve(component_spec=component_spec)
 
     assert evolved_manifest._specification == output_manifest
