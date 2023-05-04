2ac3608eedd6e0b83fa673fd6b502e8153dcd0b6
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-04 17:09:16 +0200

    Extend tests

--- tests/test_component.py
+++ tests/test_component.py
@@ -1,6 +1,8 @@
 """Fondant component tests"""
 
 import argparse
+import json
+import tempfile
 from unittest import mock 
 
 from fondant.component import FondantLoadComponent
@@ -12,15 +14,33 @@ class LoadFromHubComponent(FondantLoadComponent):
         self.spec = FondantComponentSpec.from_file("tests/example_specs/valid_component/fondant_component.yaml")
         self.args = self._add_and_parse_args()
     
-    def load(self):
+    def load(self, args):
         return -1
 
 
+# we mock the argparse arguments in order to run the tests without having to pass arguments
+metadata = json.dumps({"base_path": ".", "run_id": "200"})
 @mock.patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input_manifest_path=".", output_manifest_path="."))
+            return_value=argparse.Namespace(input_manifest_path=".", output_manifest_path=".", metadata=metadata))
 def test_component(mock_args):
     """Test that the manifest is validated correctly on instantiation"""
     component = LoadFromHubComponent()
-    assert "input_manifest_path" in component._get_component_arguments()
-    assert "output_manifest_path" in component._get_component_arguments()
-    assert argparse.Namespace(input_manifest_path=".", output_manifest_path=".") == component._add_and_parse_args()
+    
+    # test component args
+    component_args = component._get_component_arguments()
+    assert "input_manifest_path" in component_args
+    assert "output_manifest_path" in component_args
+    assert argparse.Namespace(input_manifest_path=".", output_manifest_path=".", metadata=metadata) == component._add_and_parse_args()
+    
+    # test custom args
+    component_spec_args = [arg.name for arg in component.spec.args]
+    assert component_spec_args == ["storage_args"]
+
+    # test manifest
+    initial_manifest = component._load_or_create_manifest()
+    assert initial_manifest.metadata == {'base_path': '.', 'run_id': '200', 'component_id': 'example_component'}
+
+    # Create temporary directory for writing the subsets
+    # with tempfile.TemporaryDirectory(dir="."):
+        # component.run()
+
