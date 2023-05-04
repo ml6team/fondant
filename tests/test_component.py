bb2cec36b0ec15ff9610e6b19355b55c1f5c01c3
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-04 17:20:14 +0200

    Improve tests

--- tests/test_component.py
+++ tests/test_component.py
@@ -5,23 +5,30 @@ import json
 import tempfile
 from unittest import mock 
 
+import dask.dataframe as dd
+
 from fondant.component import FondantLoadComponent
 from fondant.component_spec import FondantComponentSpec
 
 
 class LoadFromHubComponent(FondantLoadComponent):
     def __init__(self):
+        # overwrite to correctly point to the location of the yaml file
         self.spec = FondantComponentSpec.from_file("tests/example_specs/valid_component/fondant_component.yaml")
         self.args = self._add_and_parse_args()
     
     def load(self, args):
-        return -1
+        data = {"id": [0,1], "source": ["cloud", "cloud"], "captions_data": ["hello world", "this is another caption"]}
+
+        df = dd.DataFrame.from_dict(data, npartitions=1)
+        
+        return df
 
 
 # we mock the argparse arguments in order to run the tests without having to pass arguments
 metadata = json.dumps({"base_path": ".", "run_id": "200"})
 @mock.patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input_manifest_path=".", output_manifest_path=".", metadata=metadata))
+            return_value=argparse.Namespace(input_manifest_path=".", output_manifest_path="result.parquet", metadata=metadata))
 def test_component(mock_args):
     """Test that the manifest is validated correctly on instantiation"""
     component = LoadFromHubComponent()
@@ -30,7 +37,7 @@ def test_component(mock_args):
     component_args = component._get_component_arguments()
     assert "input_manifest_path" in component_args
     assert "output_manifest_path" in component_args
-    assert argparse.Namespace(input_manifest_path=".", output_manifest_path=".", metadata=metadata) == component._add_and_parse_args()
+    assert argparse.Namespace(input_manifest_path=".", output_manifest_path="result.parquet", metadata=metadata) == component._add_and_parse_args()
     
     # test custom args
     component_spec_args = [arg.name for arg in component.spec.args]
@@ -41,6 +48,6 @@ def test_component(mock_args):
     assert initial_manifest.metadata == {'base_path': '.', 'run_id': '200', 'component_id': 'example_component'}
 
     # Create temporary directory for writing the subsets
-    # with tempfile.TemporaryDirectory(dir="."):
-        # component.run()
+    with tempfile.TemporaryDirectory(dir="."):
+        component.run()
 
