5ddfa126d96fdcb32ce51f540ab4ad118e6abaa5
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-05-08 09:28:15 +0200

    add example of simple pipeline implemented iwth fondant pipelines

---
+++ examples/pipelines/simple_pipeline/simple_pipeline_fondant.py
@@ -0,0 +1,46 @@
+"""Pipeline used to create a stable diffusion dataset from a set of given images."""
+from config.general_config import KubeflowConfig
+from config.components_config import (
+    LoadFromHubConfig,
+    ImageFilterConfig,
+)
+import kfp
+
+from fondant.pipeline import FondantComponentOperation, FondantPipeline
+
+# General configs
+pipeline_name = "Test fondant pipeline"
+pipeline_description = "A test pipeline"
+pipeline_package_path = "test.tgz"
+pipeline_host = KubeflowConfig.HOST
+pipeline_base_path = KubeflowConfig.BASE_PATH
+client = kfp.Client(host=pipeline_host)
+
+# Define component ops
+load_from_hub_op = FondantComponentOperation(
+    component_spec_path="components/load_from_hub/fondant_component.yaml",
+    arguments={"dataset_name": LoadFromHubConfig.DATASET_NAME}
+
+)
+
+image_filtering_op = FondantComponentOperation(
+    component_spec_path="components/image_filtering/fondant_component.yaml",
+    arguments={"min_width": ImageFilterConfig.MIN_WIDTH,
+               "min_height": ImageFilterConfig.MIN_HEIGHT}
+
+)
+
+
+pipeline = FondantPipeline(host=pipeline_host)
+pipeline_operations = pipeline.chain_operations(load_from_hub_op, image_filtering_op)
+
+pipeline.compile_pipeline(
+    pipeline_name=pipeline_name,
+    pipeline_description=pipeline_description,
+    base_path=pipeline_base_path,
+    fondant_components_operation=pipeline_operations,
+    pipeline_package_path=pipeline_package_path
+)
+
+pipeline.upload_pipeline(pipeline_name=pipeline_name,
+                         pipeline_package_path=pipeline_package_path)
