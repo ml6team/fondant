a88f47f9ccb542ff89ec9e9a0c3af6806c42d3ab
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-10 13:35:46 +0200

    More improvements

--- examples/pipelines/controlnet-interior-design/components/retrieve_images/src/main.py
+++ examples/pipelines/controlnet-interior-design/components/retrieve_images/src/main.py
@@ -5,6 +5,7 @@ import logging
 from typing import List
 
 import dask.dataframe as dd
+import dask.array as da
 
 from clip_retrieval.clip_client import ClipClient, Modality
 
@@ -35,7 +36,7 @@ def query_clip_client(text: str, client: ClipClient) -> List[str]:
 
 class RetrieveImagesComponent(FondantTransformComponent):
     """
-    Component that retrieves images from LAION-5B based on a set of prompts.
+    Component that retrieves image URLs from LAION-5B based on a set of prompts.
     """
 
     def transform(
@@ -61,17 +62,24 @@ class RetrieveImagesComponent(FondantTransformComponent):
             indice_name="laion5B-L-14",
             num_images=num_images,
             aesthetic_score=aesthetic_score,
-            aesthetic_weight=0.5,
+            aesthetic_weight=aesthetic_weight,
             modality=Modality.IMAGE,
         )
 
-        df["images_url"] = df["prompts_text"].map_partitions(
-            lambda x: x.apply(query_clip_client, args=(client,)),
+        df["images_url"] = dataframe["prompts_text"].map_partitions(
+            lambda example: example.apply(query_clip_client, args=(client,)),
             meta=("images_url", "str"),
         )
 
         # unpack list of urls
         df = df.explode("image_urls")
+
+        # add index
+        index_list = [idx for idx in range(len(df))]
+        source_list = ["seed" for _ in range(len(df))]
+
+        df["id"] = da.array(index_list)
+        df["source"] = da.array(source_list)
 
         return df
 
