6094cbe4b0f41da04fdefa353d732607c822a488
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-05-10 10:46:00 +0200

    refactor config structure

--- examples/pipelines/simple_pipeline/simple_pipeline_fondant.py
+++ examples/pipelines/simple_pipeline/simple_pipeline_fondant.py
@@ -1,11 +1,10 @@
 """Pipeline used to create a stable diffusion dataset from a set of given images."""
 import logging
+import sys
 
-from config.general_config import KubeflowConfig
-from config.components_config import (
-    LoadFromHubConfig,
-    ImageFilterConfig,
-)
+sys.path.append('../')
+
+from pipeline_configs import PipelineConfigs
 
 from fondant.pipeline import FondantComponentOp, FondantPipeline, FondantClient
 from fondant.logger import configure_logging
@@ -15,24 +14,28 @@ logger = logging.getLogger(__name__)
 # General configs
 pipeline_name = "Test fondant pipeline"
 pipeline_description = "A test pipeline"
-pipeline_host = KubeflowConfig.HOST
-pipeline_base_path = KubeflowConfig.BASE_PATH
+pipeline_host = PipelineConfigs.HOST
+pipeline_base_path = PipelineConfigs.BASE_PATH
 
 client = FondantClient(host=pipeline_host)
 
 # Define component ops
 load_from_hub_op = FondantComponentOp(
     component_spec_path="components/load_from_hub/fondant_component.yaml",
-    arguments={"dataset_name": LoadFromHubConfig.DATASET_NAME},
+    arguments={"dataset_name": "lambdalabs/pokemon-blip-captions"},
 )
 
 image_filtering_op = FondantComponentOp(
     component_spec_path="components/image_filtering/fondant_component.yaml",
     arguments={
-        "min_width": ImageFilterConfig.MIN_WIDTH,
-        "min_height": ImageFilterConfig.MIN_HEIGHT,
+        "min_width": 600,
+        "min_height": 600,
     },
 )
+
+# TODO: ADD Arguments for embedding component later on
+# MODEL_ID = "openai/clip-vit-large-patch14"
+# BATCH_SIZE = 10
 
 pipeline = FondantPipeline(pipeline_name=pipeline_name, base_path=pipeline_base_path)
 
