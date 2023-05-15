74f227800f07479495a2cfc29bff44f92083e13a
Robbe Sneyders <robbe.sneyders@gmail.com>
2023-05-15 17:10:13 +0200

    Bugfix for release pipeline (#128)
    Fixes for the release pipeline:
    - `GITHUB_REF` contains the full ref in the format `refs/tags/<tag>`,
    while `GITHUB_REF_NAME` only contains `<tag>`
    - `set -e` in bash scripts to make the pipeline fail if the scripts
    fail.
    - Fix paths in `pre-build.sh` script

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -2,10 +2,12 @@
 # This script copies the components/ directory to fondant/components, replacing the symlink
 # It should be run before building the fondant package'
 # This script makes changes to the local files, which should not be committed to git
+set -e
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+scripts_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+root_path=$(dirname "$scripts_path")
 
-pushd "$parent_path"
+pushd "$root_path"
 rm fondant/components
 cp -r components/ fondant/
 popd
