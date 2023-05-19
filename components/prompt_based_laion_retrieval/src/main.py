cbbe02f2ba32d4879613e856a3c4b0023d1113f0
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-19 10:24:26 +0200

    Make CI green

--- components/prompt_based_laion_retrieval/src/main.py
+++ components/prompt_based_laion_retrieval/src/main.py
@@ -2,7 +2,6 @@
 This component retrieves image URLs from LAION-5B based on a set of seed prompts.
 """
 import logging
-from requests.exceptions import ConnectionError
 from typing import List
 
 import dask.dataframe as dd
@@ -32,7 +31,7 @@ def query_clip_client(text: str, client: ClipClient) -> List[str]:
     results = client.query(text=text)
     try:
         urls = [i["url"] for i in results]
-    except ConnectionError as e:
+    except:
         urls = ["" for _ in range(len(results))]
 
     return urls
