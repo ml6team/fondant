a126c99f557ef5c5c64633f715c5199c31c55f57
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-19 11:51:21 +0200

    Finish component

--- examples/pipelines/controlnet-interior-design/components/segment_images/src/main.py
+++ examples/pipelines/controlnet-interior-design/components/segment_images/src/main.py
@@ -11,15 +11,31 @@ from PIL import Image
 import dask
 import dask.dataframe as dd
 import pandas as pd
+import numpy as np
 
 from transformers import AutoImageProcessor, AutoModelForSemanticSegmentation
 import torch
+
+from palette import palette
 
 from fondant.component import TransformComponent
 from fondant.logger import configure_logging
 
 configure_logging()
 logger = logging.getLogger(__name__)
+
+
+def convert_to_rgb(seg):
+    color_seg = np.zeros(
+        (seg.shape[0], seg.shape[1], 3), dtype=np.uint8
+    )  # height, width, 3
+
+    for label, color in enumerate(palette):
+        color_seg[seg == label, :] = color
+
+    color_seg = color_seg.astype(np.uint8).tobytes()
+
+    return color_seg
 
 
 @dask.delayed
@@ -31,14 +47,18 @@ def load(example):
 
 @dask.delayed
 def transform(image, processor, device):
-    inputs = processor(images=image, return_tensors="pt").to(device)
+    inputs = processor(images=image, return_tensors="pt")
+    pixel_values = inputs.pixel_values.to(device)
 
-    return inputs
+    return pixel_values, image.size
 
 
 @dask.delayed
 def collate(examples):
-    return torch.cat([ex["pixel_values"] for ex in examples])
+    encoding = {}
+    encoding["pixel_values"] = torch.cat([ex[0] for ex in examples])
+    encoding["image_sizes"] = [ex[1] for ex in examples]
+    return encoding
 
 
 @dask.delayed
@@ -48,7 +68,8 @@ def segment(batch, model, processor):
     segmentations = processor.post_process_semantic_segmentation(
         outputs, target_sizes=batch["image_sizes"]
     )
-    segmentations = [seg.numpy() for seg in segmentations]
+    # turn into RGB images
+    segmentations = [convert_to_rgb(seg.numpy()) for seg in segmentations]
 
     return segmentations
 
