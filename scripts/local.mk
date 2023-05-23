c54a6ab37313df49eca297c5794b9cd738789789
Georges Lorré <35808396+GeorgesLorre@users.noreply.github.com>
2023-05-23 14:38:05 +0200

    Add documentation for infrastructure/getting started (#142)

---
+++ scripts/local.mk
@@ -0,0 +1,21 @@
+KUBEFLOW_PIPELINE_VERSION = 1.8.5
+
+all: start-cluster install-kfp
+
+start-cluster:
+	minikube start  --memory 8192 --cpus 4 --driver=docker
+
+install-kfp:
+	kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=${KUBEFLOW_PIPELINE_VERSION}"
+	kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+	kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=${KUBEFLOW_PIPELINE_VERSION}" && \
+	kubectl rollout status -n kubeflow deployment/ml-pipeline -w
+
+proxy-kubeflow-ui:
+	kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8005:80
+
+stop-cluster:
+	minikube stop
+
+delete:
+	minikube delete --all
