76129b8e8c657baad91fef58da3838f26199f5b2
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-06-13 16:01:17 +0200

    Use PandasTransformComponent in LAION retrieval components

--- components/embedding_based_laion_retrieval/src/main.py
+++ components/embedding_based_laion_retrieval/src/main.py
@@ -1,77 +1,42 @@
 """
-This component retrieves image URLs from LAION-5B based on a set of seed prompts.
+This component retrieves image URLs from LAION-5B based on a set of CLIP embeddings.
 """
 import asyncio
 import concurrent.futures
 import logging
 import typing as t
 
-import dask.dataframe as dd
 import pandas as pd
 
 from clip_client import ClipClient, Modality
 
-from fondant.component import TransformComponent
+from fondant.component import PandasTransformComponent
 from fondant.logger import configure_logging
 
 configure_logging()
 logger = logging.getLogger(__name__)
 
 
-def query_clip_client(embeddings: pd.Series, client: ClipClient) -> pd.DataFrame:
+class LAIONRetrievalComponent(PandasTransformComponent):
     """
-    Asynchronously retrieve image URLs and ids based on prompts in the provided dataframe.
-    Args:
-        embeddings: Series containing embedding
-        client (ClipClient): an instance of ClipClient used to query the images
-    Returns:
-        urls: A dataframe with the image urls and the LAION ids as index
+    Component that retrieves image URLs from LAION-5B based on a set of CLIP embeddings.
     """
 
-    async def async_query():
-        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-            futures = [
-                loop.run_in_executor(
-                    executor,
-                    client.query,
-                    embedding
-                )
-                for embedding in embeddings
-            ]
-            for response in await asyncio.gather(*futures):
-                results.extend(response)
-
-    results: t.List[t.Tuple[str]] = []
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(async_query())
-
-    results_df = pd.DataFrame(results)[["id", "url"]]
-    return results_df.set_index("id")
-
-
-class LAIONRetrievalComponent(TransformComponent):
-    """
-    Component that retrieves image URLs from LAION-5B based on a set of prompts.
-    """
-
-    def transform(
-        self,
-        dataframe: dd.DataFrame,
-        *,
-        num_images: int,
-        aesthetic_score: int,
-        aesthetic_weight: float
-    ) -> dd.DataFrame:
+    def setup(
+            self,
+            *,
+            num_images: int,
+            aesthetic_score: int,
+            aesthetic_weight: float
+    ) -> None:
         """
+
         Args:
-            dataframe: Dask dataframe
             num_images: number of images to retrieve for each prompt
             aesthetic_score: ranking score for aesthetic embedding, higher is prettier, between 0 and 9.
             aesthetic_weight: weight of the aesthetic embedding to add to the query, between 0 and 1.
-        Returns:
-            Dask dataframe
         """
-        client = ClipClient(
+        self.client = ClipClient(
             url="https://knn.laion.ai/knn-service",
             indice_name="laion5B-L-14",
             num_images=num_images,
@@ -80,19 +45,40 @@ class LAIONRetrievalComponent(TransformComponent):
             modality=Modality.IMAGE,
         )
 
-        meta_df = pd.DataFrame(columns=["id", "url"], dtype="string")
-        meta_df = meta_df.set_index("id")
+    def transform(
+            self,
+            dataframe: pd.DataFrame,
+    ) -> pd.DataFrame:
+        """
+        Args:
+            dataframe: Dask dataframe
 
-        dataframe = dataframe["embeddings_data"].map_partitions(
-            query_clip_client,
-            client=client,
-            meta=meta_df
-        )
+        Returns:
+            Dask dataframe
+        """
+        results: t.List[t.Tuple[str]] = []
+        loop = asyncio.new_event_loop()
 
-        dataframe = dataframe.rename(columns={"url": "images_url"})
+        async def async_query():
+            with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+                futures = [
+                    loop.run_in_executor(
+                        executor,
+                        self.client.query,
+                        embedding
+                    )
+                    for embedding in dataframe["embeddings_data"]
+                ]
+                for response in await asyncio.gather(*futures):
+                    results.extend(response)
 
-        return dataframe
+        loop.run_until_complete(async_query())
 
+        results_df = pd.DataFrame(results)[["id", "url"]]
+        results_df.rename(columns={"url": "images_url"}, inplace=True)
+        results_df.set_index("id", inplace=True)
+
+        return results_df
 
 
 if __name__ == "__main__":
