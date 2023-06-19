a8c1ec5fce865cbe22501edaa5fa4ff8f076592f
Georges Lorre <georges.lorre@ml6.eu>
2023-06-19 16:48:28 +0200

    Add cli entrypoint for compile

--- fondant/cli.py
+++ fondant/cli.py
@@ -121,3 +121,99 @@ def run_data_explorer():
     logging.info(f"Access the explorer at http://localhost:{args.port}")
 
     subprocess.call(cmd, stdout=subprocess.PIPE)  # nosec
+import argparse
+import importlib.util
+import re
+
+from fondant.compiler import DockerCompiler
+from fondant.pipeline import Pipeline
+
+
+class PipelinePythonReference(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super().__init__(option_strings, dest, nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        module, object = self.parse_input(values)
+        pipeline = self.load_pipeline(module, object)
+        setattr(namespace, self.dest, pipeline)
+
+    @staticmethod
+    def parse_input(input) -> tuple:
+        """
+        Parse a string input into a tuple of (module, object).
+
+        Args:
+            input: the input string to parse
+        Returns:
+            A tuple of (module, object)
+        """
+        pattern = re.compile(r"(\S*):(\S*)")
+        match = re.match(pattern, input)
+        if match:
+            return match.groups()
+        else:
+            raise ValueError(f"Could not parse input {input}")
+
+    @staticmethod
+    def load_pipeline(module: str, object: str) -> Pipeline:
+        """Attempt to load a pipeline from a module file path and object name."""
+        spec = importlib.util.spec_from_file_location(object, module)
+        pipeline = importlib.util.module_from_spec(spec)  # type: ignore
+        spec.loader.exec_module(pipeline)  # type: ignore
+
+        if isinstance(pipeline.pipeline, Pipeline):
+            return pipeline.pipeline
+        else:
+            raise ValueError(f"Could not load pipeline from {module}:{object}")
+
+
+def compile(args):
+    """Compile a pipeline based on the passed arguments."""
+    pipeline = args.pipeline
+    if args.mode == "local":
+        compiler = DockerCompiler()
+        compiler.compile(pipeline, output_path=args.output_path)
+    elif args.mode == "kubeflow":
+        pass
+
+
+def run(args):
+    pass
+
+
+def main():
+    main_parser = argparse.ArgumentParser()
+    subparsers = main_parser.add_subparsers()
+
+    compile_parser = subparsers.add_parser("compile")
+    compile_parser.add_argument(
+        "-p", "--pipeline", type=str, required=True, action=PipelinePythonReference
+    )
+    compile_parser.add_argument(
+        "-m, --mode", choices=["local", "kubeflow"], default="local", dest="mode"
+    )
+    compile_parser.add_argument(
+        "--output_path",
+        type=str,
+        required=False,
+        default="docker-compose.yml",
+        dest="output_path",
+    )
+    compile_parser.add_argument(
+        "--extra_volumes", required=False, action="append", dest="extra_volumes"
+    )
+    compile_parser.set_defaults(func=compile)
+
+    # TODO: Implement run
+    # run_parser = subparsers.add_parser("run")
+    # run_parser.set_defaults(func=run)
+
+    args = main_parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
