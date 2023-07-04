794d7ae461323dae7215919526f9fe9bc640f13f
mrchtr <matthias.r1092@gmail.com>
2023-07-04 17:02:07 +0200

    Removing configure_logger

--- components/text_normalization/src/main.py
+++ components/text_normalization/src/main.py
@@ -7,9 +7,7 @@ from typing import List
 import pandas as pd
 
 from fondant.component import PandasTransformComponent
-from fondant.logger import configure_logging
 
-configure_logging()
 logger = logging.getLogger(__name__)
 
 
