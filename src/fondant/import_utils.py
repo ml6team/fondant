f266f4ae7eb891caa89c5b2580fadf961633a4f7
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-07-17 09:50:20 +0200

    Remove obsolete packages (#293)
    PR that removes obsolete extra packages from `import_utils`

--- src/fondant/import_utils.py
+++ src/fondant/import_utils.py
@@ -7,23 +7,9 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-PANDAS_IMPORT_ERROR = """
-`{0}` requires the pandas library but it was not found in your environment. Please install fondant
- using the 'pandas' extra.
-"""
-
-DATASETS_IMPORT_ERROR = """
-`{0}` requires the 🤗 Datasets library but it was not found in your environment.
-Please install fondant using the 'datasets' extra.
-Note that if you have a local folder named `datasets` or a local python file named
- `datasets.py` in your current working directory, python may try to import this instead of the 🤗
- Datasets library. You should rename this folder or that python file if that's the case.
-  Please note that you may need to restart your runtime after installation.
-"""
-
 KFP_IMPORT_ERROR = """
 `{0}` requires the kubeflow pipelines (kfp) library but it was not found in your environment.
-Please install fondant using the 'pipelines' extra.
+Please install fondant using pip install fondant [pipelines].
 """
 
 
@@ -48,16 +34,6 @@ def is_package_available(package_name: str, import_error_msg: str) -> bool:
         return package_available
 
     raise ModuleNotFoundError(import_error_msg.format(Path(sys.argv[0]).stem))
-
-
-def is_datasets_available():
-    """Check if 'datasets' is available."""
-    return is_package_available("datasets", DATASETS_IMPORT_ERROR)
-
-
-def is_pandas_available():
-    """Check if 'pandas' is available."""
-    return is_package_available("pandas", PANDAS_IMPORT_ERROR)
 
 
 def is_kfp_available():
