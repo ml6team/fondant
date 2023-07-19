6fbc83b2f48ed2f60fad494e38b9b46eeb9e1940
Niels Rogge <niels.rogge1@gmail.com>
2023-07-19 10:48:30 +0200

    More improvements

--- examples/pipelines/datacomp/pipeline.py
+++ examples/pipelines/datacomp/pipeline.py
@@ -7,14 +7,13 @@ sys.path.append("../")
 
 from pipeline_configs import PipelineConfigs
 
-from fondant.compiler import DockerCompiler
 from fondant.pipeline import ComponentOp, Pipeline, Client
 
 logger = logging.getLogger(__name__)
 
 # Initialize pipeline and client
 pipeline = Pipeline(
-    pipeline_name="Datacomp filtering pipeline",
+    pipeline_name="datacomp",
     pipeline_description="A pipeline for filtering the Datacomp dataset",
     # base_path=PipelineConfigs.BASE_PATH,
     base_path="/Users/nielsrogge/Documents/fondant_artifacts_datacomp",
@@ -62,9 +61,8 @@ cluster_image_embeddings_op = ComponentOp(
         "num_clusters": 100,
     },
 )
-dedup_image_embeddings_op = ComponentOp.from_registry(
-    name="dedup_image_embeddings",
-    component_spec_path="components/dedup_image_embeddings/fondant_component.yaml",
+dedup_image_embeddings_op = ComponentOp(
+    component_dir="components/dedup_image_embeddings",
     arguments={
         "epsilon": 0.03,
     },
@@ -72,18 +70,8 @@ dedup_image_embeddings_op = ComponentOp.from_registry(
 
 # add ops to pipeline
 pipeline.add_op(load_from_hub_op)
-pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
-pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
-pipeline.add_op(cluster_image_embeddings_op, dependencies=filter_complexity_op)
+# pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
+# pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
+pipeline.add_op(cluster_image_embeddings_op, dependencies=load_from_hub_op)
 pipeline.add_op(dedup_image_embeddings_op, dependencies=cluster_image_embeddings_op)
 # TODO add more ops
-
-# compile
-if __name__ == "__main__":
-    compiler = DockerCompiler()
-    # mount the gcloud credentials to the container
-    extra_volumes = [
-        "$HOME/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json:ro"
-    ]
-    compiler.compile(pipeline=pipeline, extra_volumes=extra_volumes)
-    logger.info("Run `docker compose up` to run the pipeline.")
