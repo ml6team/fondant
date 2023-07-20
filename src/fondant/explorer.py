3088b21bd7370bca78b0694d63169153d0ea309f
Matthias Richter <matthias.r1092@gmail.com>
2023-07-20 20:42:39 +0200

    Bugfix: data explorer artifact mounting (#310)
    During the last fix I have incorporate a new bug in mounting the correct
    path to the docker container.
    I have adapted the implementation. The path in `fondant explore
    --data-directory <path>` can also be an relative path now.
    The path will be resolved to an absolute path. Inside the docker
    container the folder will be located in the root folder.
    
    ---------
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Robbe Sneyders <robbe.sneyders@gmail.com>

--- src/fondant/explorer.py
+++ src/fondant/explorer.py
@@ -1,7 +1,9 @@
 import logging
+import os
 import shlex
 import subprocess  # nosec
 import typing as t
+from pathlib import Path
 
 DEFAULT_CONTAINER = "ghcr.io/ml6team/data_explorer"
 DEFAULT_TAG = "latest"
@@ -41,9 +43,12 @@ def run_explorer_app(
 
     # mount the local data directory to the container
     if data_directory:
-        print(data_directory)
+        data_directory_path = Path(data_directory).resolve()
+        host_machine_path = str(data_directory_path)
+        container_path = os.path.join("/", data_directory_path.name)
+
         cmd.extend(
-            ["-v", f"{shlex.quote(data_directory)}:{shlex.quote(data_directory)}"],
+            ["-v", f"{shlex.quote(host_machine_path)}:{shlex.quote(container_path)}"],
         )
 
     # add the image name
