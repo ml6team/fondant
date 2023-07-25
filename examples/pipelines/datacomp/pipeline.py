cd1c7fcf675a6de8d107d4fa51cbdc50f2697176
Niels Rogge <niels.rogge1@gmail.com>
2023-07-25 16:13:05 +0200

    More improvements

--- examples/pipelines/datacomp/pipeline.py
+++ examples/pipelines/datacomp/pipeline.py
@@ -38,7 +38,7 @@ load_from_hub_op = ComponentOp(
     arguments={
         "dataset_name": "nielsr/datacomp-small-with-embeddings",
         "column_name_mapping": load_component_column_mapping,
-        "n_rows_to_load": 100,
+        "n_rows_to_load": 500000,
     },
 )
 filter_image_resolution_op = ComponentOp.from_registry(
@@ -64,7 +64,9 @@ cluster_image_embeddings_op = ComponentOp(
 
 # add ops to pipeline
 pipeline.add_op(load_from_hub_op)
-pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
-pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
-pipeline.add_op(cluster_image_embeddings_op, dependencies=filter_complexity_op)
+# pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
+# pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
+# pipeline.add_op(cluster_image_embeddings_op, dependencies=filter_complexity_op)
 # TODO add more ops
+
+# client.compile_and_run(pipeline=pipeline)
