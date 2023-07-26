9bba3ca744f93cff2f26ade184d854db0fcccbac
SATISH J <satish27may@gmail.com>
2023-07-26 17:25:10 +0530

    Add abastract test class for component testing

--- components/caption_images/tests/test_caption_images.py
+++ components/caption_images/tests/test_caption_images.py
@@ -1,56 +1,66 @@
-import pandas as pd
+import inspect
+
 import pytest
-import requests
-import torch
-from caption_images.src.main import CaptionImagesComponent, caption_image_batch, process_image
-from transformers import BlipForConditionalGeneration, BlipProcessor
+from caption_images.src.main import CaptionImagesComponent
+from fondant.component import (
+    BaseComponent,
+    DaskLoadComponent,
+    DaskTransformComponent,
+    DaskWriteComponent,
+    PandasTransformComponent,
+)
 
 
-class TestImageProcessing:
+def get_base_class_name(class_object):
+    return class_object.__base__.__name__
+component_class_map = {
+    "BaseComponent": BaseComponent,
+    "DaskLoadComponent": DaskLoadComponent,
+    "DaskTransformComponent": DaskTransformComponent,
+    "DaskWriteComponent": DaskWriteComponent,
+    "PandasTransformComponent": PandasTransformComponent,
+}
 
-    @pytest.fixture(autouse=True)
-    def __setup_teardown(self):
-        # Setup code: Create instance of BlipProcessor
-        self.model_id = 'Salesforce/blip-image-captioning-base'
-        self.processor = BlipProcessor.from_pretrained(self.model_id)
-        self.model = BlipForConditionalGeneration.from_pretrained(self.model_id).to('cpu')
 
-        self.image_data = {
-            'images': {
-                'data': [],
-            },
-        }
+class Test_AbstractComponent:
+    @pytest.fixture(
+        scope="class", params=[CaptionImagesComponent],
+    )  # move the component classes here
+    def get_base_component(self, request):
+        """
+        Method to get the base component class
+        of the component_object.
+        """
+        component_object = request.param
+        base_component_object = component_class_map[
+            get_base_class_name(component_object)
+        ]
+        return base_component_object, component_object
 
-        image_urls = ["https://cdn.pixabay.com/photo/2023/06/29/09/52/angkor-thom-8096092_960_720.jpg",
-                      "https://cdn.pixabay.com/photo/2023/07/19/18/56/japanese-beetle-8137606_960_720.png"]
+    def test_component(self, get_base_component):
+        base_component_object, component_object = get_base_component
+        methods = [
+            method
+            for method in dir(base_component_object)
+            if not method.startswith("_")
+            and callable(getattr(base_component_object, method))
+            and not inspect.isbuiltin(getattr(base_component_object, method))
+        ]
+        # check if component object has these methods
+        for method_name in methods:
+            assert callable(
+                getattr(component_object, method_name),
+            ), f"{component_object.__class__.__name__} should have {method_name} method"
 
-        for url in image_urls:
-            response = requests.get(url)
-            self.image_data['images']['data'].append(response.content)
+        attributes = [
+            attr
+            for attr in dir(base_component_object)
+            if not attr.startswith("_")
+            and not callable(getattr(base_component_object, attr))
+        ]
 
-        self.df = pd.DataFrame(self.image_data)
-
-        # Add teardown code if any
-
-    def test_process_image(self):
-        img_byte_arr = self.image_data['images']['data'][0]
-        output = process_image(img_byte_arr, processor=self.processor, device='cpu')
-        assert isinstance(output, torch.Tensor)
-
-    def test_caption_image_batch(self):
-        image_batch = pd.Series(self.df['images']['data']).apply(process_image,
-                                                      processor=self.processor,
-                                                      device='cpu')
-
-        captions = caption_image_batch(image_batch, model=self.model,
-                                       processor=self.processor, max_new_tokens=40)
-
-        assert isinstance(captions, pd.Series)
-        assert captions.shape[0] == image_batch.shape[0]
-
-    def test_CaptionImagesComponent_transform(self):
-        component = CaptionImagesComponent(model_id=self.model_id, batch_size=5, max_new_tokens=40)
-        result_df = component.transform(self.df)
-
-        assert isinstance(result_df, pd.DataFrame)
-        assert ('captions', 'text') in result_df.columns
+        # check if component object has these attributes
+        for attribute in attributes:
+            assert getattr(
+                component_object, attribute,
+            ), f"{component_object.__class__.__name__} should have {attribute} attribute"
