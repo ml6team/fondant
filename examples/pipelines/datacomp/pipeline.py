ce67179cd3674bf606e58ed8e2fb20b50975a0d3
Niels Rogge <niels.rogge1@gmail.com>
2023-08-01 10:45:44 +0200

    Add download images component

--- examples/pipelines/datacomp/pipeline.py
+++ examples/pipelines/datacomp/pipeline.py
@@ -67,11 +67,15 @@ cluster_image_embeddings_op = ComponentOp(
         "num_clusters": 3,
     },
 )
+download_images_op = ComponentOp(
+    component_dir="components/download_images",
+)
 
 # add ops to pipeline
 pipeline.add_op(load_from_hub_op)
-pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
-pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
+# pipeline.add_op(filter_image_resolution_op, dependencies=load_from_hub_op)
+# pipeline.add_op(filter_complexity_op, dependencies=filter_image_resolution_op)
+pipeline.add_op(download_images_op, dependencies=load_from_hub_op)
 # pipeline.add_op(cluster_image_embeddings_op, dependencies=filter_complexity_op)
 # TODO add more ops
 
