bdc093d794002204c871cff75f9bedd4d6a3534e
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-08-16 16:58:46 +0200

    Use httpx client in download_images component

--- components/download_images/src/main.py
+++ components/download_images/src/main.py
@@ -6,89 +6,16 @@ Some functions here are directly taken from
 https://github.com/rom1504/img2dataset/blob/main/img2dataset/downloader.py.
 """
 import asyncio
-import concurrent.futures
-import functools
-import io
 import logging
-import traceback
 import typing as t
-import urllib
 
+import httpx
 import pandas as pd
 from fondant.component import PandasTransformComponent
 from fondant.executor import PandasTransformExecutor
 from resizer import Resizer
 
 logger = logging.getLogger(__name__)
-
-
-def is_disallowed(headers, user_agent_token, disallowed_header_directives):
-    """Check if HTTP headers contain an X-Robots-Tag directive disallowing usage."""
-    for values in headers.get_all("X-Robots-Tag", []):
-        try:
-            uatoken_directives = values.split(":", 1)
-            directives = [x.strip().lower() for x in uatoken_directives[-1].split(",")]
-            ua_token = (
-                uatoken_directives[0].lower() if len(uatoken_directives) == 2  # noqa: PLR2004
-                else None
-            )
-            if (ua_token is None or ua_token == user_agent_token) and any(
-                    x in disallowed_header_directives for x in directives
-            ):
-                return True
-        except Exception as err:
-            traceback.print_exc()
-            print(f"Failed to parse X-Robots-Tag: {values}: {err}")
-    return False
-
-
-def download_image(url, timeout, user_agent_token, disallowed_header_directives):
-    """Download an image with urllib."""
-    img_stream = None
-    user_agent_string = (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
-    )
-    if user_agent_token:
-        user_agent_string += f" (compatible; {user_agent_token}; " \
-                             f"+https://github.com/ml6team/fondant)"
-    try:
-        request = urllib.request.Request(
-            url, data=None, headers={"User-Agent": user_agent_string},
-        )
-        with urllib.request.urlopen(request, timeout=timeout) as r:
-            if disallowed_header_directives and is_disallowed(
-                    r.headers,
-                    user_agent_token,
-                    disallowed_header_directives,
-            ):
-                return None
-            img_stream = io.BytesIO(r.read())
-        return img_stream
-    except Exception:
-        if img_stream is not None:
-            img_stream.close()
-        return None
-
-
-def download_image_with_retry(
-        id_,
-        url,
-        *,
-        timeout,
-        retries,
-        resizer,
-        user_agent_token=None,
-        disallowed_header_directives=None,
-):
-    for _ in range(retries + 1):
-        img_stream = download_image(
-            url, timeout, user_agent_token, disallowed_header_directives,
-        )
-        if img_stream is not None:
-            # resize the image
-            img_str, width, height = resizer(img_stream)
-            return id_, img_str, width, height
-    return id_, None, None, None
 
 
 class DownloadImagesComponent(PandasTransformComponent):
@@ -128,33 +55,46 @@ class DownloadImagesComponent(PandasTransformComponent):
             max_aspect_ratio=max_aspect_ratio,
         )
 
+    async def download_image(self, url: str) -> t.Optional[bytes]:
+        user_agent_string = (
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
+        )
+        user_agent_string += " (compatible; +https://github.com/ml6team/fondant)"
+
+        transport = httpx.AsyncHTTPTransport(retries=self.retries)
+        async with httpx.AsyncClient(transport=transport) as client:
+            try:
+                response = await client.get(url, timeout=self.timeout)
+                image_stream = response.content
+            except Exception as e:
+                logger.warning(f"Skipping {url}: {e}")
+                image_stream = None
+
+        return image_stream
+
+    async def download_and_resize_image(self, id_: str, url: str) \
+            -> t.Tuple[str, t.Optional[bytes], t.Optional[int], t.Optional[int]]:
+        image_stream = await self.download_image(url)
+        if image_stream is not None:
+            image_stream, width, height = self.resizer(image_stream)
+        else:
+            image_stream, width, height = None, None, None
+        return id_, image_stream, width, height
+
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
 
         logger.info(f"Downloading {len(dataframe)} images...")
 
-        results: t.List[t.Tuple[str]] = []
-        loop = asyncio.new_event_loop()
+        results: t.List[t.Tuple[str, bytes, int, int]] = []
 
-        async def async_download():
-            with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-                futures = [
-                    loop.run_in_executor(
-                        executor,
-                        functools.partial(
-                            download_image_with_retry,
-                            id_,
-                            url,
-                            timeout=self.timeout,
-                            retries=self.retries,
-                            resizer=self.resizer,
-                        ),
-                    )
-                    for id_, url in zip(dataframe.index, dataframe["images"]["url"])
-                ]
-                for response in await asyncio.gather(*futures):
-                    results.append(response)
+        async def download_dataframe() -> None:
+            images = await asyncio.gather(
+                *[self.download_and_resize_image(id_, url)
+                  for id_, url in zip(dataframe.index, dataframe["images"]["url"])],
+            )
+            results.extend(images)
 
-        loop.run_until_complete(async_download())
+        asyncio.run(download_dataframe())
 
         results_df = pd.DataFrame(results)
         results_df.columns = ["id", ("images", "data"), ("images", "width"), ("images", "height")]
