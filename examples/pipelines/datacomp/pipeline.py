7e4f8d48d0c616314ed3e189b69c0813011c8bee
Niels Rogge <niels.rogge1@gmail.com>
2023-08-22 15:19:03 +0200

    More fixes

--- examples/pipelines/datacomp/pipeline.py
+++ examples/pipelines/datacomp/pipeline.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 pipeline = Pipeline(
     pipeline_name="datacomp-filtering-pipeline",
     pipeline_description="A pipeline for filtering the Datacomp dataset",
-    base_path=PipelineConfigs.BASE_PATH,
+    # base_path=PipelineConfigs.BASE_PATH,
+    base_path="/Users/nielsrogge/Documents/fondant_artifacts_datacomp",
 )
 
 # define ops
@@ -35,7 +36,7 @@ load_from_hub_op = ComponentOp(
     arguments={
         "dataset_name": "mlfoundations/datacomp_small",
         "column_name_mapping": load_component_column_mapping,
-        "n_rows_to_load": 10000,
+        "n_rows_to_load": 100,
         "index_column": "uid",
     },
     node_pool_label="node_pool",
