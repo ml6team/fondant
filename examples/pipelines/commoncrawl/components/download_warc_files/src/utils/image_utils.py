6f8d908c4231785afab8c2b3e87f66f9abf0452b
Robbe Sneyders <robbe.sneyders@gmail.com>
2023-08-29 11:30:54 +0200

    Add commoncrawl pipeline that starts from warc paths (#392)
    This PR adds a commoncrawl pipeline which contains 2 components:
    - A component that downloads `warc.paths.gz` files and outputs all the
    paths as separate partitions
    - A component that downloads a single warc file and extracts the images
    and its licenses
    
    ---------
    
    Co-authored-by: Sharon Grundmann <sharon.grundmann@ml6.eu>

--- examples/pipelines/commoncrawl/components/download_warc_files/src/utils/image_utils.py
+++ examples/pipelines/commoncrawl/components/download_warc_files/src/utils/image_utils.py
@@ -1,6 +1,6 @@
 import logging
 from urllib.parse import urlparse
-from typing import Any, List
+import typing as t
 
 logger = logging.getLogger(__name__)
 
@@ -13,11 +13,14 @@ def get_full_image_url(image_url: str, webpage_url: str) -> str:
     Returns:
         The full image url.
     """
-    if "http" not in image_url or image_url[0] == "/":
+    if image_url.startswith("http") is False:
         parsed_webpage_url = urlparse(webpage_url)
-        image_url = (
-            f"{parsed_webpage_url.scheme}://{parsed_webpage_url.netloc}{image_url}"
-        )
+        if image_url.startswith("//"):
+            image_url = f"{parsed_webpage_url.scheme}:{image_url}"
+        else:
+            image_url = (
+                f"{parsed_webpage_url.scheme}://{parsed_webpage_url.netloc}{image_url}"
+            )
 
         try:
             pos = image_url.index("?")
@@ -29,8 +32,8 @@ def get_full_image_url(image_url: str, webpage_url: str) -> str:
 
 
 def get_image_info(
-    a_tag: Any, webpage_url: str, license_type: str, license_location: str
-) -> List[str]:
+    a_tag: t.Any, webpage_url: str, license_type: str, license_location: str
+) -> t.Optional[t.Tuple[str, str, str, str, str]]:
     """Returns the image url, alt text, webpage url, and license type.
     Args:
         a_tag: The parsed html code.
@@ -43,15 +46,15 @@ def get_image_info(
 
     if img_tag and img_tag.has_attr("src"):
         img_src = get_full_image_url(img_tag["src"], webpage_url)
-        img_alt = img_tag.get("alt", "")
-        return [img_src, img_alt, webpage_url, license_type, license_location]
+        img_alt: str = img_tag.get("alt", "")
+        return img_src, img_alt, webpage_url, license_type, license_location
 
     return None
 
 
 def get_images_from_soup(
-    soup: Any, webpage_url: str, license_type: str, license_location: str
-) -> List[List[str]]:
+    soup: t.Any, webpage_url: str, license_type: str, license_location: str
+) -> t.List[t.Tuple[str, str, str, str, str]]:
     """Returns a list of image urls from the parsed html code.
     Args:
         soup: The parsed html code.
@@ -69,15 +72,17 @@ def get_images_from_soup(
     return image_info
 
 
-def get_unique_images(images: List[List[str]]) -> List[List[str]]:
+def get_unique_images(
+    images: t.List[t.Tuple[str, str, str, str, str]]
+) -> t.List[t.Tuple[str, str, str, str, str]]:
     """Returns a list of unique images.
     Args:
         images: A list of images.
     Returns:
         A list of unique images.
     """
-    unique_images = []
+    unique_images = set()
     for image in images:
         if image not in unique_images:
-            unique_images.append(image)
-    return unique_images
+            unique_images.add(image)
+    return list(unique_images)
