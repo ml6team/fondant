c3bdbf3ea14d84abea9c9a5ca85d9dede3a87d5e
Niels Rogge <niels.rogge1@gmail.com>
2023-08-30 15:24:02 +0200

    More updates

--- examples/pipelines/datacomp/pipeline.py
+++ examples/pipelines/datacomp/pipeline.py
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 pipeline = Pipeline(
     pipeline_name="datacomp-filtering-pipeline",
     pipeline_description="A pipeline for filtering the Datacomp dataset",
-    # base_path=PipelineConfigs.BASE_PATH,
-    base_path="/Users/nielsrogge/Documents/fondant_artifacts_datacomp",
+    base_path=PipelineConfigs.BASE_PATH,
+    # base_path="/Users/nielsrogge/Documents/fondant_artifacts_datacomp",
 )
 
 # define ops
@@ -63,7 +63,7 @@ detect_text_op = ComponentOp(
     },
     node_pool_label="node_pool",
     node_pool_name="model-inference-pool",
-    # number_of_gpus=1,
+    number_of_gpus=1,
     cache=False,
 )
 mask_images_op = ComponentOp(
@@ -79,7 +79,7 @@ embed_images_op = ComponentOp.from_registry(
     },
     node_pool_label="node_pool",
     node_pool_name="model-inference-pool",
-    # number_of_gpus=1,
+    number_of_gpus=1,
     cache=False,
 )
 add_clip_score_op = ComponentOp(
