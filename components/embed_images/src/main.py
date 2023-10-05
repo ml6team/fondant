69e9789223be49d00a94e9d70377be326a046eaa
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-10-05 11:15:17 +0200

    optimize GPU components

--- components/embed_images/src/main.py
+++ components/embed_images/src/main.py
@@ -1,27 +1,33 @@
 """This component that embeds images using a model from the Hugging Face hub."""
 import io
 import logging
+import os
 import typing as t
 
-import dask
 import numpy as np
 import pandas as pd
 import torch
 from fondant.component import PandasTransformComponent
 from PIL import Image
-from transformers import CLIPProcessor, CLIPVisionModelWithProjection
+from transformers import BatchEncoding, CLIPProcessor, CLIPVisionModelWithProjection
 
 logger = logging.getLogger(__name__)
 
-dask.config.set(scheduler="single-threaded")
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ['TORCH_CUDNN_V8_API_DISABLED'] = "1"
 
 
-def process_image(image: bytes, *, processor: CLIPProcessor, device: str) -> torch.Tensor:
+def process_image_batch(
+        images: np.ndarray,
+        *,
+        processor: CLIPProcessor,
+        device: str,
+) -> t.List[torch.Tensor]:
     """
-    Process the image to a tensor.
+    Process image in batches to a list of tensors.
 
     Args:
-        image: The input image as a byte string.
+        images: The input images as a numpy array containing byte strings.
         processor: The processor object for transforming the image.
         device: The device to move the transformed image to.
     """
@@ -31,23 +37,31 @@ def process_image(image: bytes, *, processor: CLIPProcessor, device: str) -> tor
         bytes_ = io.BytesIO(img)
         return Image.open(bytes_).convert("RGB")
 
-    def transform(img: Image) -> torch.Tensor:
+    def transform(img: Image) -> BatchEncoding:
         """Transform the image to a tensor using a clip processor and move it to the specified
         device.
         """
+        # Edge case: https://github.com/huggingface/transformers/issues/21638
+        if img.width == 1 or img.height == 1:
+            img = img.resize((224, 224))
+
         return processor(images=img, return_tensors="pt").to(device)
 
-    return transform(load(image))["pixel_values"]
+    return [transform(load(image))["pixel_values"] for image in images]
 
 
 @torch.no_grad()
-def embed_image_batch(image_batch: pd.DataFrame, *, model: CLIPVisionModelWithProjection) -> \
-        pd.Series:
+def embed_image_batch(
+        image_batch: t.List[torch.Tensor],
+        *,
+        model: CLIPVisionModelWithProjection,
+        index: pd.Series,
+) -> pd.Series:
     """Embed a batch of images."""
-    input_batch = torch.cat(image_batch.tolist())
+    input_batch = torch.cat(image_batch)
     output_batch = model(input_batch)
     embeddings_batch = output_batch.image_embeds.cpu().tolist()
-    return pd.Series(embeddings_batch, index=image_batch.index)
+    return pd.Series(embeddings_batch, index=index)
 
 
 class EmbedImagesComponent(PandasTransformComponent):
@@ -75,14 +89,22 @@ class EmbedImagesComponent(PandasTransformComponent):
         self.batch_size = batch_size
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        images = dataframe["images"]["data"].apply(
-            process_image,
-            processor=self.processor,
-            device=self.device,
-        )
+        images = dataframe["images"]["data"]
+
         results: t.List[pd.Series] = []
         for batch in np.split(images, np.arange(self.batch_size, len(images), self.batch_size)):
             if not batch.empty:
-                results.append(embed_image_batch(batch, model=self.model).T)
+                image_tensors = process_image_batch(
+                    batch,
+                    processor=self.processor,
+                    device=self.device,
+                )
+                embeddings = embed_image_batch(
+                    image_tensors,
+                    model=self.model,
+                    index=batch.index,
+                ).T
+                results.append(embeddings)
+
 
         return pd.concat(results).to_frame(name=("embeddings", "data"))
