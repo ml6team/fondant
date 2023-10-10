4691718553db48b4f58408fe454b54fc53068e06
Matthias Richter <matthias.r1092@gmail.com>
2023-10-10 10:49:05 +0200

    Use s3 client instead of http to access common crawl (#501)
    Update the Common Crawl download and image extraction component to
    utilise S3 instead of HTTP requests.
    
    Solving #497

--- examples/pipelines/commoncrawl/components/extract_images_from_warc/src/utils/download_utils.py
+++ examples/pipelines/commoncrawl/components/extract_images_from_warc/src/utils/download_utils.py
@@ -4,7 +4,8 @@ import requests
 from requests import Session
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
-
+import boto3
+import botocore
 
 logger = logging.getLogger(__name__)
 
@@ -12,27 +13,14 @@ COMMONCRAWL_BASE_URL = "https://data.commoncrawl.org/"
 
 
 def download_warc_file(
-    warc_file: str, retries: int = 10, backoff_factor: int = 5
-) -> requests.Response:
-    """Downloads a WARC file using http requests.
-    Args:
-        warc_file: The path to the WARC file.
-    Returns:
-        The WARC file as a bytes object.
-    """
-    session = Session()
-    retry_strategy = Retry(
-        total=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=[502, 503, 504],
-        allowed_methods={"POST", "GET"},
+    warc_file: str,
+    retries: int = 10,
+):
+    retry_config = botocore.config.Config(
+        retries={"max_attempts": retries, "mode": "standard"}
     )
-    session.mount("https://", HTTPAdapter(max_retries=retry_strategy))
 
-    try:
-        response = session.get(COMMONCRAWL_BASE_URL + warc_file, stream=True)
-        response.raise_for_status()
-        return response
-    except Exception as e:
-        logger.error(f"Error downloading WARC file: {e}")
-        raise
+    s3_client = boto3.client("s3", config=retry_config)
+    bucket_name = "commoncrawl"
+    response = s3_client.get_object(Bucket=bucket_name, Key=warc_file)
+    return response["Body"]
