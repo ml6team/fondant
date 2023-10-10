ml6team/fondant

commit 47dcda82cbcca924f1e737037291cfafa7694dad

Restructure-cli (#488)
With the introduction of a new compiler and runner for vertex, I feel
the need to restructure the CLI a bit. This is a draft PR (docs and
tests still need to be updated).

**Running**

* Before

```bash
fondant run pipeline.py --local
```

* After

Mode is not longer a positional argument, Pipeline/reference is moved to
the end (similar to docker).

```bash
 fondant run local pipeline.py 
```
**Help**

* Before 

Arguments are not isolated per mode which makes it which argument
belongs to which mode (e.g. extra volumes not needed for KFP)
```
❯ fondant run -h                 
usage: fondant run [-h] (--local | --kubeflow) [--output-path OUTPUT_PATH] [--extra-volumes EXTRA_VOLUMES [EXTRA_VOLUMES ...]] [--build-arg BUILD_ARG] [--host HOST] ref

positional arguments:
  ref                   Reference to the pipeline to run, can be a path to a spec file or a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)

options:
  -h, --help            show this help message and exit
  --local
  --kubeflow
  --output-path OUTPUT_PATH, -o OUTPUT_PATH
                        Output directory
  --extra-volumes EXTRA_VOLUMES [EXTRA_VOLUMES ...]
                        Extra volumes to mount in containers
  --build-arg BUILD_ARG
                        Build arguments to pass to `docker build`. Format {key}={value}.
  --host HOST           KubeFlow pipeline host url
```
* After

```bash
❯fondant run -h  

positional arguments:
  {local,kubeflow}
    local           Local runner
    kubeflow        Kubeflow runner
  ref               Reference to the pipeline to run, can be a path to a spec file or a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)

options:
  -h, --help        show this help message and exit
```

To further deep dive 

```bash
❯ fondant run local -h

src/fondant/cli.py
@@ -80,20 +80,6 @@ def entrypoint():
     args.func(args)
 
 
-def set_default_output(args: argparse.Namespace):
-    """Set the default output path depending on the runner type."""
-    if args.output_path is None:
-        if args.local:
-            args.output_path = "docker-compose.yml"
-        elif args.kubeflow:
-            args.output_path = "pipeline.yaml"
-        else:
-            msg = "One of the arguments --local --kubeflow is required"
-            raise ValueError(msg)
-
-    return args
-
-
 def register_explore(parent_parser):
     parser = parent_parser.add_parser(
         "explore",
@@ -184,24 +170,23 @@ def register_compile(parent_parser):
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent(
             """
-        Compile a fondant pipeline into either a docker-compose.yml(local) or kubeflow spec file.
+        Compile a fondant pipeline into pipeline specification file file.
 
         The pipeline argument is a formatstring. The compiler will try to import the pipeline from the module specified in the formatstring.
         (NOTE: path is patched to include the current working directory so you can do relative imports)
 
-        The --local or --kubeflow flag specifies the mode in which the pipeline will be compiled.
-        You can use the --extra-volumes flag to specify extra volumes to mount in the containers this can be used:
+        You can use different modes for fondant runners. Current existing modes are local and kubeflow.
 
-        - to mount data directories to be used by the pipeline (note that if your pipeline's base_path is local it will already be mounted for you).
-        - to mount cloud credentials (see examples))
+        Examples of compiling component:
+        fondant compile local --extra-volumes $HOME/.aws/credentials:/root/.aws/credentials my_project.my_pipeline.py
 
-        Example:
-        fondant compile my_project.my_pipeline.py --local --extra-volumes $HOME/.aws/credentials:/root/.aws/credentials
-
-        fondant compile my_project.my_pipeline.py --kubeflow --extra-volumes $HOME/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json
+        fondant compile kubeflow --extra-volumes $HOME/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json my_project.my_pipeline.py
         """,
         ),
     )
+
+    compiler_subparser = parser.add_subparsers()
+
     parser.add_argument(
         "ref",
         help="""Reference to the pipeline to run, can be a path to a spec file or
@@ -209,47 +194,61 @@ def register_compile(parent_parser):
             """,
         action="store",
     )
-    # add a mutually exclusive group for the mode
-    mode_group = parser.add_mutually_exclusive_group(required=True)
-    mode_group.add_argument("--local", action="store_true")
-    mode_group.add_argument("--kubeflow", action="store_true")
 
-    parser.add_argument(
+    local_parser = compiler_subparser.add_parser(name="local", help="Local compiler")
+    kubeflow_parser = compiler_subparser.add_parser(
+        name="kubeflow",
+        help="Kubeflow compiler",
+    )
+
+    # Local runner parser
+    local_parser.add_argument(
         "--output-path",
         "-o",
-        help="Output directory",
-        default=None,
+        help="Output path of compiled pipeline",
+        default="docker-compose.yml",
     )
-    parser.add_argument(
+    local_parser.add_argument(
         "--extra-volumes",
-        help="Extra volumes to mount in containers",
+        help="""Extra volumes to mount in containers. You can use the --extra-volumes flag to specify extra volumes to mount in the containers this can be used:
+        - to mount data directories to be used by the pipeline (note that if your pipeline's base_path is local it will already be mounted for you).
+        - to mount cloud credentials""",
         nargs="+",
     )
-    parser.add_argument(
+    local_parser.add_argument(
         "--build-arg",
         action="append",
         help="Build arguments to pass to `docker build`. Format {key}={value}.",
         default=[],
     )
 
-    parser.set_defaults(func=compile)
+    # Kubeflow parser
+    kubeflow_parser.add_argument(
+        "--output-path",
+        "-o",
+        help="Output path of compiled pipeline",
+        default="pipeline.yaml",
+    )
+
+    local_parser.set_defaults(func=compile_local)
+    kubeflow_parser.set_defaults(func=compile_kfp)
 
 
-def compile(args):
-    args = set_default_output(args)
+def compile_local(args):
     pipeline = pipeline_from_module(args.ref)
+    compiler = DockerCompiler()
+    compiler.compile(
+        pipeline=pipeline,
+        extra_volumes=args.extra_volumes,
+        output_path=args.output_path,
+        build_args=args.build_arg,
+    )
 
-    if args.local:
-        compiler = DockerCompiler()
-        compiler.compile(
-            pipeline=pipeline,
-            extra_volumes=args.extra_volumes,
-            output_path=args.output_path,
-            build_args=args.build_arg,
-        )
-    elif args.kubeflow:
-        compiler = KubeFlowCompiler()
-        compiler.compile(pipeline=pipeline, output_path=args.output_path)
+
+def compile_kfp(args):
+    pipeline = pipeline_from_module(args.ref)
+    compiler = KubeFlowCompiler()
+    compiler.compile(pipeline=pipeline, output_path=args.output_path)
 
 
 def register_run(parent_parser):
@@ -262,15 +261,18 @@ def register_run(parent_parser):
         pipeline (see fondant compile --help for more info)
         OR a path to a spec file in which case it will compile the pipeline first and then run it.
 
-        The --local or --kubeflow flag specifies the mode in which the pipeline will be ran.
-        You can use the --extra-volumes flag to specify extra volumes to mount in the containers this can be used:
+        You can use different modes for fondant runners. Current existing modes are `local` and `kubeflow`.
+        You can run `fondant <mode> --help` to find out more about the specific arguments for each mode.
 
-        Example:
-        fondant run my_project.my_pipeline.py --local --extra-volumes $HOME/.aws/credentials:/root/.aws/credentials
-        fondant run ./my_compiled_kubeflow_pipeline.tgz --kubeflow
+        Examples of running component:
+        fondant run local --extra-volumes $HOME/.aws/credentials:/root/.aws/credentials my_project.my_pipeline.py
+        fondant run kubeflow ./my_compiled_kubeflow_pipeline.tgz
         """,
         ),
     )
+
+    runner_subparser = parser.add_subparsers()
+    # Define the "ref" argument once
     parser.add_argument(
         "ref",
         help="""Reference to the pipeline to run, can be a path to a spec file or
@@ -278,72 +280,89 @@ def register_run(parent_parser):
             """,
         action="store",
     )
-    # add a mutually exclusive group for the mode
-    mode_group = parser.add_mutually_exclusive_group(required=True)
-    mode_group.add_argument("--local", action="store_true")
-    mode_group.add_argument("--kubeflow", action="store_true")
 
-    parser.add_argument(
+    local_parser = runner_subparser.add_parser(name="local", help="Local runner")
+    kubeflow_parser = runner_subparser.add_parser(
+        name="kubeflow",
+        help="Kubeflow runner",
+    )
+
+    # Local runner parser
+    local_parser.add_argument(
         "--output-path",
         "-o",
-        help="Output directory",
-        default=None,
+        help="Output path of compiled pipeline",
+        default="docker-compose.yml",
     )
-    parser.add_argument(
+    local_parser.add_argument(
         "--extra-volumes",
-        help="Extra volumes to mount in containers",
         nargs="+",
+        help="""Extra volumes to mount in containers. You can use the --extra-volumes flag to specify extra volumes to mount in the containers this can be used:
+        - to mount data directories to be used by the pipeline (note that if your pipeline's base_path is local it will already be mounted for you).
+        - to mount cloud credentials""",
     )
-    parser.add_argument(
+    local_parser.add_argument(
         "--build-arg",
         action="append",
-        help="Build arguments to pass to `docker build`. Format {key}={value}.",
+        help="Build arguments for `docker build`",
     )
-    parser.add_argument("--host", help="KubeFlow pipeline host url", required=False)
-    parser.set_defaults(func=run)
+    local_parser.set_defaults(func=run_local)
+
+    # kubeflow runner parser
+    kubeflow_parser.add_argument(
+        "--output-path",
+        "-o",
+        help="Output path of compiled pipeline",
+        default="pipeline.yaml",
+    )
+    kubeflow_parser.add_argument(
+        "--host",
+        help="KubeFlow pipeline host url",
+        required=True,
+    )
+
+    kubeflow_parser.set_defaults(func=run_kfp)
 
 
-def run(args):
-    args = set_default_output(args)
+def run_local(args):
+    try:
+        pipeline = pipeline_from_module(args.ref)
+    except ModuleNotFoundError:
+        spec_ref = args.ref
+    else:
+        spec_ref = args.output_path
+        logging.info(
+            "Found reference to un-compiled pipeline... compiling to {spec_ref}",
+        )
+        compiler = DockerCompiler()
+        compiler.compile(
+            pipeline=pipeline,
+            extra_volumes=args.extra_volumes,
+            output_path=spec_ref,
+            build_args=args.build_arg,
+        )
+    finally:
+        DockerRunner().run(spec_ref)
 
-    if args.local:
-        try:
-            pipeline = pipeline_from_module(args.ref)
-        except ModuleNotFoundError:
-            spec_ref = args.ref
-        else:
-            spec_ref = args.output_path
-            logging.info(
-                "Found reference to un-compiled pipeline... compiling to {spec_ref}",
-            )
-            compiler = DockerCompiler()
-            compiler.compile(
-                pipeline=pipeline,
-                extra_volumes=args.extra_volumes,
-                output_path=spec_ref,
-                build_args=args.build_arg,
-            )
-        finally:
-            DockerRunner().run(spec_ref)
 
-    elif args.kubeflow:
-        if not args.host:
-            msg = "--host argument is required for running on Kubeflow"
-            raise ValueError(msg)
-        try:
-            pipeline = pipeline_from_module(args.ref)
-        except ModuleNotFoundError:
-            spec_ref = args.ref
-        else:
-            spec_ref = args.output_path
-            logging.info(
-                f"Found reference to un-compiled pipeline... compiling to {spec_ref}",
-            )
-            compiler = KubeFlowCompiler()
-            compiler.compile(pipeline=pipeline, output_path=spec_ref)
-        finally:
-            runner = KubeflowRunner(host=args.host)
-            runner.run(input_spec=spec_ref)
+def run_kfp(args):
+    if not args.host:
+        msg = "--host argument is required for running on Kubeflow"
+        raise ValueError(msg)
+    try:
+        pipeline = pipeline_from_module(args.ref)
+    except ModuleNotFoundError:
+        spec_ref = args.ref
+    else:
+        spec_ref = args.output_path
+        logging.info(
+            "Found reference to un-compiled pipeline... compiling to {spec_ref}",
+        )
+        compiler = KubeFlowCompiler()
+        compiler.compile(pipeline=pipeline, output_path=spec_ref)
+    finally:
+        runner = KubeflowRunner(host=args.host)
+        runner.run(input_spec=spec_ref)
 
 
 def register_execute(parent_parser):

tests/test_cli.py
@@ -6,12 +6,14 @@ import pytest
 from fondant.cli import (
     ComponentImportError,
     PipelineImportError,
-    compile,
+    compile_kfp,
+    compile_local,
     component_from_module,
     execute,
     get_module,
     pipeline_from_module,
-    run,
+    run_kfp,
+    run_local,
 )
 from fondant.component import DaskLoadComponent
 from fondant.executor import Executor, ExecutorFactory
@@ -137,7 +139,7 @@ def test_local_logic(tmp_path_factory):
             extra_volumes=[],
             build_arg=[],
         )
-        compile(args)
+        compile_local(args)
 
 
 def test_kfp_compile(tmp_path_factory):
@@ -148,14 +150,14 @@ def test_kfp_compile(tmp_path_factory):
             local=False,
             output_path=str(fn / "kubeflow_pipelines.yml"),
         )
-        compile(args)
+        compile_kfp(args)
 
 
 def test_local_run(tmp_path_factory):
     """Test that the run command works with different arguments."""
     args = argparse.Namespace(local=True, ref="some/path", output_path=None)
     with patch("subprocess.call") as mock_call:
-        run(args)
+        run_local(args)
         mock_call.assert_called_once_with(
             [
                 "docker",
@@ -178,7 +180,7 @@ def test_local_run(tmp_path_factory):
             extra_volumes=[],
             build_arg=[],
         )
-        run(args1)
+        run_local(args1)
         mock_call.assert_called_once_with(
             [
                 "docker",
@@ -207,7 +209,7 @@ def test_kfp_run(tmp_path_factory):
         ValueError,
         match="--host argument is required for running on Kubeflow",
     ):  # no host
-        run(args)
+        run_kfp(args)
     with patch("fondant.cli.KubeflowRunner") as mock_runner:
         args = argparse.Namespace(
             kubeflow=True,
@@ -216,7 +218,7 @@ def test_kfp_run(tmp_path_factory):
             host="localhost",
             ref="some/path",
         )
-        run(args)
+        run_kfp(args)
         mock_runner.assert_called_once_with(host="localhost")
     with patch("fondant.cli.KubeflowRunner") as mock_runner, tmp_path_factory.mktemp(
         "temp",
@@ -228,5 +230,5 @@ def test_kfp_run(tmp_path_factory):
             output_path=str(fn / "kubeflow_pipelines.yml"),
             ref=__name__,
         )
-        run(args)
+        run_kfp(args)
         mock_runner.assert_called_once_with(host="localhost2")

commit 8faccb46b3df9347f4176d5d6d4d4526bbda3e6c

Fix run CLI (#507)
The `run` command was broken in #488. This fixes it by adding the `ref`
argument to the subparsers separately.

src/fondant/cli.py
@@ -187,14 +187,6 @@ def register_compile(parent_parser):
 
     compiler_subparser = parser.add_subparsers()
 
-    parser.add_argument(
-        "ref",
-        help="""Reference to the pipeline to run, can be a path to a spec file or
-            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
-            """,
-        action="store",
-    )
-
     local_parser = compiler_subparser.add_parser(name="local", help="Local compiler")
     kubeflow_parser = compiler_subparser.add_parser(
         name="kubeflow",
@@ -202,6 +194,13 @@ def register_compile(parent_parser):
     )
 
     # Local runner parser
+    local_parser.add_argument(
+        "ref",
+        help="""Reference to the pipeline to run, can be a path to a spec file or
+            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
+            """,
+        action="store",
+    )
     local_parser.add_argument(
         "--output-path",
         "-o",
@@ -223,6 +222,13 @@ def register_compile(parent_parser):
     )
 
     # Kubeflow parser
+    kubeflow_parser.add_argument(
+        "ref",
+        help="""Reference to the pipeline to run, can be a path to a spec file or
+            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
+            """,
+        action="store",
+    )
     kubeflow_parser.add_argument(
         "--output-path",
         "-o",
@@ -272,14 +278,6 @@ def register_run(parent_parser):
     )
 
     runner_subparser = parser.add_subparsers()
-    # Define the "ref" argument once
-    parser.add_argument(
-        "ref",
-        help="""Reference to the pipeline to run, can be a path to a spec file or
-            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
-            """,
-        action="store",
-    )
 
     local_parser = runner_subparser.add_parser(name="local", help="Local runner")
     kubeflow_parser = runner_subparser.add_parser(
@@ -288,6 +286,13 @@ def register_run(parent_parser):
     )
 
     # Local runner parser
+    local_parser.add_argument(
+        "ref",
+        help="""Reference to the pipeline to run, can be a path to a spec file or
+            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
+            """,
+        action="store",
+    )
     local_parser.add_argument(
         "--output-path",
         "-o",
@@ -309,6 +314,13 @@ def register_run(parent_parser):
     local_parser.set_defaults(func=run_local)
 
     # kubeflow runner parser
+    kubeflow_parser.add_argument(
+        "ref",
+        help="""Reference to the pipeline to run, can be a path to a spec file or
+            a module containing the pipeline instance that will be compiled first (e.g. pipeline.py)
+            """,
+        action="store",
+    )
     kubeflow_parser.add_argument(
         "--output-path",
         "-o",