0f8cde2021cd6c6a0d4c17925ba9758a66346910
Matthias Richter <matthias.r1092@gmail.com>
2023-10-11 13:17:37 +0200

    Remove abstract component test

--- components/caption_images/tests/test_caption_images.py
+++ components/caption_images/tests/test_caption_images.py
@@ -1,27 +1,31 @@
 import pandas as pd
 import requests
-from caption_images.src.main import CaptionImagesComponent
-from fondant.abstract_component_test import AbstractComponentTest
+
+from src.main import CaptionImagesComponent
 
 
-class TestCaptionImagesComponent(AbstractComponentTest):
-    def create_component(self):
-        return CaptionImagesComponent(
+def test_image_caption_component():
+    image_urls = [
+        "https://cdn.pixabay.com/photo/2023/06/29/09/52/angkor-thom-8096092_1280.jpg",
+        "https://cdn.pixabay.com/photo/2023/07/19/18/56/japanese-beetle-8137606_1280.png",
+    ]
+    input_dataframe = pd.DataFrame(
+        {"images": {"data": [requests.get(url).content for url in image_urls]}})
+
+    expected_output_dataframe = pd.DataFrame(
+            data={("captions", "text"): {0: "a motorcycle", 1: "a beetle"}},
+        )
+
+    component = CaptionImagesComponent(
             model_id="Salesforce/blip-image-captioning-base",
             batch_size=4,
             max_new_tokens=2,
         )
 
-    def create_input_data(self):
-        image_urls = [
-            "https://cdn.pixabay.com/photo/2023/06/29/09/52/angkor-thom-8096092_1280.jpg",
-            "https://cdn.pixabay.com/photo/2023/07/19/18/56/japanese-beetle-8137606_1280.png",
-        ]
-        return pd.DataFrame(
-            {"images": {"data": [requests.get(url).content for url in image_urls]}},
-        )
+    output_dataframe = component.transform(input_dataframe)
 
-    def create_output_data(self):
-        return pd.DataFrame(
-            data={("captions", "text"): {0: "a motorcycle", 1: "a beetle"}},
-        )
+    pd.testing.assert_frame_equal(
+        left=expected_output_dataframe,
+        right=output_dataframe,
+        check_dtype=False,
+    )
