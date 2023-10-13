5b24d757c604f24a8bebbb249e55aee45f6145f8
tillwenke <till.wenke@gmx.net>
2023-10-13 18:25:41 +0200

    describe args

--- components/generate_embeddings/src/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def secrets_to_env_vars():
-    #adjust here to get the secrets from wherever they are stored
-    secrets ={"api":"key"}
-    for key, value in secrets.items():
-        os.environ[key] = value
