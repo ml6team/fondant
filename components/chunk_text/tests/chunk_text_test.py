8f22e204ef3b46b8d1c3762e646718feb09a9650
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-10-18 14:06:41 +0200

    Add tests

---
+++ components/chunk_text/tests/chunk_text_test.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+from src.main import ChunkTextComponent
+
+
+def test_transform():
+    """Test chunk component method."""
+    input_dataframe = pd.DataFrame(
+        {
+            ("text", "data"): [
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo",
+                "ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis",
+                "parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,",
+            ],
+        },
+        index=pd.Index(["a", "b", "c"], name="id"),
+    )
+
+    component = ChunkTextComponent(
+        chunk_size=50,
+        chunk_overlap=20,
+    )
+
+    output_dataframe = component.transform(input_dataframe)
+
+    expected_output_dataframe = pd.DataFrame(
+        {
+            ("text", "original_document_id"): ["a", "a", "a", "b", "b", "c", "c"],
+            ("text", "data"): [
+                "Lorem ipsum dolor sit amet, consectetuer",
+                "amet, consectetuer adipiscing elit. Aenean",
+                "elit. Aenean commodo",
+                "ligula eget dolor. Aenean massa. Cum sociis",
+                "massa. Cum sociis natoque penatibus et magnis dis",
+                "parturient montes, nascetur ridiculus mus. Donec",
+                "mus. Donec quam felis, ultricies nec,",
+            ],
+        },
+        index=pd.Index(["a_0", "a_1", "a_2", "b_0", "b_1", "c_0", "c_1"], name="id"),
+    )
+
+    pd.testing.assert_frame_equal(
+        left=output_dataframe,
+        right=expected_output_dataframe,
+        check_dtype=False,
+    )
