772c628a8cbe74f416a5cf525c963f35fc1a2f5c
Matthias Richter <matthias.r1092@gmail.com>
2023-11-07 15:14:52 +0100

    Add dummy component

---
+++ tests/example_pipelines/executable_pipeline/components/dummy_component/src/main.py
@@ -0,0 +1,24 @@
+"""
+Component that chunks text into smaller segments.
+
+This component takes a body of text and chunks into small chunks. The id of the returned dataset
+consists of the id of the original document followed by the chunk index.
+
+"""
+import logging
+
+import pandas as pd
+from fondant.component import PandasTransformComponent
+
+logger = logging.getLogger(__name__)
+
+
+class DummyComponent(PandasTransformComponent):
+    """Dummy component that returns the dataframe as it is."""
+
+    def __init__(self, *_):
+        pass
+
+    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Dummy component that returns the dataframe as it is."""
+        return dataframe
