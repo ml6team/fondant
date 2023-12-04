d6ebaf46a8d3577d3ddb9c578817cfbe05a7fac7
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-12-04 10:02:05 +0100

    add tests to manifest evolution

--- tests/core/test_manifest_evolution.py
+++ tests/core/test_manifest_evolution.py
@@ -1,12 +1,34 @@
 import json
 from pathlib import Path
 
+import pyarrow as pa
 import pytest
 import yaml
 from fondant.core.component_spec import ComponentSpec
 from fondant.core.manifest import Manifest
+from fondant.core.schema import Field, Type
 
 examples_path = Path(__file__).parent / "examples/evolution_examples"
+
+TEST_MANIFESTS = {
+    "1": {"produces": None},
+    "2": {"produces": None},
+    "3": {"produces": None},
+    "4": {"produces": None},
+    # Generic component
+    "5": {
+        "produces": {
+            "embedding_data": Field("embedding_data", Type(pa.list_(pa.float32()))),
+        },
+    },
+    # Non-generic component (output mapping)
+    "6": {
+        "produces": {
+            "images_data": "images_array",
+            "text_data": "text_string",
+        },
+    },
+}
 
 
 @pytest.fixture()
@@ -18,20 +40,26 @@ def input_manifest():
 def examples():
     """Returns examples as tuples of component and expected output_manifest."""
     for directory in (f for f in examples_path.iterdir() if f.is_dir()):
+        example_name = directory.stem
+        produces = TEST_MANIFESTS[example_name]["produces"]
         with open(directory / "component.yaml") as c, open(
             directory / "output_manifest.json",
         ) as o:
-            yield yaml.safe_load(c), json.load(o)
+            yield yaml.safe_load(c), json.load(o), produces
 
 
-@pytest.mark.parametrize(("component_spec", "output_manifest"), list(examples()))
-def test_evolution(input_manifest, component_spec, output_manifest):
+@pytest.mark.parametrize(
+    ("component_spec", "output_manifest", "produces"),
+    list(examples()),
+)
+def test_evolution(input_manifest, component_spec, output_manifest, produces):
     run_id = "custom_run_id"
     manifest = Manifest(input_manifest)
     component_spec = ComponentSpec(component_spec)
     evolved_manifest = manifest.evolve(
         component_spec=component_spec,
         run_id=run_id,
+        produces=produces,
     )
 
     assert evolved_manifest._specification == output_manifest
