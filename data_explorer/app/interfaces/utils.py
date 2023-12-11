0cf89568ee6bc0efd3046ed70975050530ff3e92
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-12-11 13:47:58 +0100

    Fix indexerror on run selection

--- data_explorer/app/interfaces/utils.py
+++ data_explorer/app/interfaces/utils.py
@@ -5,7 +5,8 @@ import streamlit as st
 
 def get_default_index(key: str, option_list: list) -> int:
     """Get the default index for a selectbox based on previous selection from session state."""
-    if st.session_state[key] is None:
+    selected_option = st.session_state.get(key)
+    if selected_option is None or selected_option not in option_list:
         return 0
 
-    return option_list.index(st.session_state[key])
+    return option_list.index(selected_option)
