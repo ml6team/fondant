1a6a8a030a34f7a59bdc9bf99a348d9de07be7b8
Shubham Krishna <shubham.krishna@ml6.eu>
2023-12-21 17:39:40 +0100

    Fix docstrings, failing tests, add tqdm

--- components/index_aws_opensearch/src/main.py
+++ components/index_aws_opensearch/src/main.py
@@ -1,22 +1,27 @@
-from typing import Any, Dict
+import logging
+from typing import Any, Dict, Optional
 
 import boto3
 import dask.dataframe as dd
 from fondant.component import DaskWriteComponent
 from opensearchpy import AWSV4SignerAuth, OpenSearch, RequestsHttpConnection
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
 class IndexAWSOpenSearchComponent(DaskWriteComponent):
     def __init__(
         self,
+        *,
         host: str,
         region: str,
         index_name: str,
         index_body: Dict[str, Any],
-        port: int = 443,
-        use_ssl: bool = True,
-        verify_certs: bool = True,
-        pool_maxsize: int = 20,
+        port: Optional[int],
+        use_ssl: Optional[bool],
+        verify_certs: Optional[bool],
+        pool_maxsize: Optional[int],
         **kwargs,
     ):
         session = boto3.Session()
@@ -35,26 +40,26 @@ class IndexAWSOpenSearchComponent(DaskWriteComponent):
         self.create_index(index_body)
 
     def create_index(self, index_body: Dict[str, Any]):
-        """Creates an index in AWS OpenSearch.
+        """Creates an index if not existing in AWS OpenSearch.
 
         Args:
-            index_body (Dict[str, Any]): Parameters that specify index settings,
+            index_body: Parameters that specify index settings,
             mappings, and aliases for newly created index.
         """
-        self.client.indices.create(self.index_name, body=index_body)
+        if self.client.indices.exists(index=self.index_name):
+            logger.info(f"Index: {self.index_name} already exists.")
+        else:
+            logger.info(f"Creating Index: {self.index_name} with body: {index_body}")
+            self.client.indices.create(self.index_name, body=index_body)
 
     def write(self, dataframe: dd.DataFrame):
         """
         Writes the data from the given Dask DataFrame to AWS OpenSearch Index.
 
         Args:
-            dataframe (dd.DataFrame): The Dask DataFrame containing the data to be written.
+            dataframe: The Dask DataFrame containing the data to be written.
         """
-        if not self.client.indices.exists(index=self.index_name):
-            msg = f"Index: {self.index_name} doesn't exist. Please Create"
-            raise ValueError(msg)
-
-        for part in dataframe.partitions:
+        for part in tqdm(dataframe.partitions):
             df = part.compute()
             for row in df.itertuples():
                 body = {"embedding": row.embedding, "text": row.text}
