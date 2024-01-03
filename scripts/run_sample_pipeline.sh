317826a64423900533e55a157d5e427abf59d01b
Matthias Richter <matthias.r1092@gmail.com>
2024-01-03 15:27:59 +0100

    Fix ci/cd

--- scripts/run_sample_pipeline.sh
+++ scripts/run_sample_pipeline.sh
@@ -15,4 +15,15 @@ cleanup() {
 
 trap cleanup EXIT
 
-poetry run fondant run local examples/sample_pipeline.py
+poetry run fondant run local examples/sample_pipeline_test/sample_pipeline.py
+
+# Expect that .artifacts was created and isn't empty
+if [ -d "./examples/sample_pipeline_test/.artifacts" ]; then
+    if [ "$(ls -A .artifacts)" ]; then
+        echo "Sample pipeline executed successfully."
+        exit 0
+    fi
+fi
+
+echo "Sample pipeline execution failed."
+exit 1
