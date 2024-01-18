697bae8b43374b952c800bb2783bd56d582a4aa0
Philippe Moussalli <philippe.moussalli95@gmail.com>
2024-01-18 14:26:21 +0100

    enable reranking

--- components/retrieve_from_weaviate/src/main.py
+++ components/retrieve_from_weaviate/src/main.py
@@ -1,5 +1,6 @@
 import typing as t
 
+import dask.dataframe as dd
 import pandas as pd
 import weaviate
 from fondant.component import PandasTransformComponent
@@ -16,6 +17,7 @@ class RetrieveFromWeaviateComponent(PandasTransformComponent):
         additional_headers: t.Optional[dict],
         hybrid_query: t.Optional[str],
         hybrid_alpha: t.Optional[float],
+        rerank: bool,
         **kwargs,
     ) -> None:
         """
@@ -29,6 +31,10 @@ class RetrieveFromWeaviateComponent(PandasTransformComponent):
             hybrid_query: The hybrid query to be used for retrieval. Optional parameter.
             hybrid_alpha: Argument to change how much each search affects the results. An alpha
              of 1 is a pure vector search. An alpha of 0 is a pure keyword search.
+            rerank: Whether to rerank the results based on the hybrid query. Defaults to False.
+             Check this notebook for more information on reranking:
+             https://github.com/weaviate/recipes/blob/main/ranking/cohere-ranking/cohere-ranking.ipynb
+             https://weaviate.io/developers/weaviate/search/rerank.
         """
         # Initialize your component here based on the arguments
         self.client = weaviate.Client(
@@ -42,6 +48,7 @@ class RetrieveFromWeaviateComponent(PandasTransformComponent):
             hybrid_query,
             hybrid_alpha,
         )
+        self.rerank = rerank
 
     @staticmethod
     def validate_hybrid_query(
@@ -58,10 +65,20 @@ class RetrieveFromWeaviateComponent(PandasTransformComponent):
 
         return hybrid_query, hybrid_alpha
 
+    def validate_reranker(self, dataframe: dd.DataFrame) -> None:
+        if self.rerank and "prompt" not in dataframe.columns:
+            msg = (
+                "If rerank is specified, dataframe must contain a 'text' column. Reranking is"
+                " only supported for text data and not for embeddings."
+            )
+            raise ValueError(
+                msg,
+            )
+
     def teardown(self) -> None:
         del self.client
 
-    def retrieve_chunks(self, vector_query: list):
+    def retrieve_chunks_from_embeddings(self, vector_query: list):
         """Get results from weaviate database."""
         query = (
             self.client.query.get(self.class_name, ["passage"])
@@ -77,8 +94,42 @@ class RetrieveFromWeaviateComponent(PandasTransformComponent):
         result_dict = result["data"]["Get"][self.class_name]
         return [retrieved_chunk["passage"] for retrieved_chunk in result_dict]
 
-    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe["retrieved_chunks"] = dataframe["embedding"].apply(
-            self.retrieve_chunks,
+    def retrieve_chunks_from_prompts(self, prompt: str):
+        """Get results from weaviate database."""
+        query = (
+            self.client.query.get(self.class_name, ["passage"])
+            .with_near_text({"concepts": [prompt]})
+            .with_limit(self.k)
         )
+        if self.hybrid_query is not None:
+            query = query.with_hybrid(query=self.hybrid_query, alpha=self.hybrid_alpha)
+
+        if self.rerank:
+            query = query.with_additional(
+                'rerank(property: "passage" query: "prompt") { score }',
+            )
+
+        result = query.do()
+
+        result_dict = result["data"]["Get"][self.class_name]
+        return [retrieved_chunk["passage"] for retrieved_chunk in result_dict]
+
+    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        self.validate_reranker(dataframe)
+
+        if "embedding" in dataframe.columns:
+            dataframe["retrieved_chunks"] = dataframe["embedding"].apply(
+                self.retrieve_chunks_from_embeddings,
+            )
+
+        elif "prompt" in dataframe.columns:
+            dataframe["retrieved_chunks"] = dataframe["prompt"].apply(
+                self.retrieve_chunks_from_prompts,
+            )
+        else:
+            msg = "Dataframe must contain either an 'embedding' column or a 'prompt' column."
+            raise ValueError(
+                msg,
+            )
+
         return dataframe
