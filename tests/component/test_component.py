86991d41aec99ad7069f379042ecf5f3c128fac0
Philippe Moussalli <philippe.moussalli95@gmail.com>
2024-01-25 16:51:37 +0100

    new approach

--- tests/component/test_component.py
+++ tests/component/test_component.py
@@ -70,6 +70,11 @@ def _patched_data_writing(monkeypatch):
         "upload_manifest",
         lambda self, manifest, save_path: None,
     )
+    monkeypatch.setattr(
+        Executor,
+        "_upload_cache_reference_content",
+        lambda self, base_path, pipeline_name: None,
+    )
 
 
 def patch_method_class(method):
@@ -528,7 +533,7 @@ def test_wrap_transform():
     assert output_df.columns.tolist() == ["caption_text", "image_height"]
 
 
-@pytest.mark.usefixtures("_patched_data_loading")
+@pytest.mark.usefixtures("_patched_data_loading", "_patched_data_writing")
 def test_write_component(metadata):
     operation_spec = OperationSpec(
         ComponentSpec.from_file(components_path / "component.yaml"),
