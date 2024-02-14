e577fd5a5d22fe277faba76574c05848b4a23b91
Robbe Sneyders <robbe.sneyders@ml6.eu>
2024-02-14 10:53:39 +0100

    Update embed_images component to use multiple gpu

--- src/fondant/components/embed_images/src/main.py
+++ src/fondant/components/embed_images/src/main.py
@@ -7,6 +7,8 @@ import typing as t
 import numpy as np
 import pandas as pd
 import torch
+from dask.distributed import Client, get_worker
+from dask_cuda import LocalCUDACluster
 from fondant.component import PandasTransformComponent
 from PIL import Image
 from transformers import BatchEncoding, CLIPProcessor, CLIPVisionModelWithProjection
@@ -14,53 +16,6 @@ from transformers import BatchEncoding, CLIPProcessor, CLIPVisionModelWithProjec
 logger = logging.getLogger(__name__)
 
 os.environ["TORCH_CUDNN_V8_API_DISABLED"] = "1"
-
-
-def process_image_batch(
-    images: np.ndarray,
-    *,
-    processor: CLIPProcessor,
-    device: str,
-) -> t.List[torch.Tensor]:
-    """
-    Process image in batches to a list of tensors.
-
-    Args:
-        images: The input images as a numpy array containing byte strings.
-        processor: The processor object for transforming the image.
-        device: The device to move the transformed image to.
-    """
-
-    def load(img: bytes) -> Image:
-        """Load the bytestring as an image."""
-        bytes_ = io.BytesIO(img)
-        return Image.open(bytes_).convert("RGB")
-
-    def transform(img: Image) -> BatchEncoding:
-        """Transform the image to a tensor using a clip processor and move it to the specified
-        device.
-        """
-        # Edge case: https://github.com/huggingface/transformers/issues/21638
-        if img.width == 1 or img.height == 1:
-            img = img.resize((224, 224))
-
-        return processor(images=img, return_tensors="pt").to(device)
-
-    return [transform(load(image))["pixel_values"] for image in images]
-
-
-@torch.no_grad()
-def embed_image_batch(
-    image_batch: t.List[torch.Tensor],
-    *,
-    model: CLIPVisionModelWithProjection,
-    index: pd.Series,
-) -> pd.Series:
-    """Embed a batch of images."""
-    input_batch = torch.cat(image_batch)
-    output_batch = model(input_batch)
-    embeddings_batch = output_batch.image_embeds.cpu().tolist()
-    return pd.Series(embeddings_batch, index=index)
 
 
 class EmbedImagesComponent(PandasTransformComponent):
@@ -76,19 +31,82 @@ class EmbedImagesComponent(PandasTransformComponent):
         Args:
             model_id: id of the model on the Hugging Face hub
             batch_size: batch size to use.
-            kwargs: Unhandled keyword arguments passed in by Fondant.
         """
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        logger.info("device used is %s", self.device)
-
-        logger.info("Initialize model '%s'", model_id)
-        self.processor = CLIPProcessor.from_pretrained(model_id)
-        self.model = CLIPVisionModelWithProjection.from_pretrained(model_id).to(
-            self.device,
-        )
-        logger.info("Model initialized")
-
+        self.model_id = model_id
         self.batch_size = batch_size
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        logger.info("Using device '%s'", self.device)
+
+        super().__init__()
+
+    def dask_client(self) -> Client:
+        if self.device == "cuda":
+            cluster = LocalCUDACluster()
+            return Client(cluster)
+
+        return super().dask_client()
+
+    def process_image_batch(self, images: np.ndarray) -> t.List[torch.Tensor]:
+        """
+        Process image in batches to a list of tensors.
+
+        Args:
+            images: The input images as a numpy array containing byte strings.
+        """
+        worker = get_worker()
+
+        if hasattr(worker, "processor"):
+            processor = worker.processor
+        else:
+            logger.info(
+                "Initializing processor for '%s' on worker '%s",
+                self.model_id,
+                worker,
+            )
+            processor = CLIPProcessor.from_pretrained(self.model_id)
+            worker.processor = processor
+
+        def load(img: bytes) -> Image:
+            """Load the bytestring as an image."""
+            bytes_ = io.BytesIO(img)
+            return Image.open(bytes_).convert("RGB")
+
+        def transform(img: Image) -> BatchEncoding:
+            """Transform the image to a tensor using a clip processor and move it to the specified
+            device.
+            """
+            # Edge case: https://github.com/huggingface/transformers/issues/21638
+            if img.width == 1 or img.height == 1:
+                img = img.resize((224, 224))
+
+            return processor(images=img, return_tensors="pt").to(self.device)
+
+        return [transform(load(image))["pixel_values"] for image in images]
+
+    @torch.no_grad()
+    def embed_image_batch(
+        self,
+        image_batch: t.List[torch.Tensor],
+        *,
+        index: pd.Series,
+    ) -> pd.Series:
+        """Embed a batch of images."""
+        worker = get_worker()
+
+        if hasattr(worker, "model"):
+            model = worker.model
+        else:
+            logger.info("Initializing model '%s' on worker '%s", self.model_id, worker)
+            model = CLIPVisionModelWithProjection.from_pretrained(self.model_id).to(
+                self.device,
+            )
+            worker.model = model
+
+        input_batch = torch.cat(image_batch)
+        output_batch = model(input_batch)
+        embeddings_batch = output_batch.image_embeds.cpu().tolist()
+        return pd.Series(embeddings_batch, index=index)
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         images = dataframe["image"]
@@ -99,14 +117,11 @@ class EmbedImagesComponent(PandasTransformComponent):
             np.arange(self.batch_size, len(images), self.batch_size),
         ):
             if not batch.empty:
-                image_tensors = process_image_batch(
+                image_tensors = self.process_image_batch(
                     batch,
-                    processor=self.processor,
-                    device=self.device,
                 )
-                embeddings = embed_image_batch(
+                embeddings = self.embed_image_batch(
                     image_tensors,
-                    model=self.model,
                     index=batch.index,
                 ).T
                 results.append(embeddings)
