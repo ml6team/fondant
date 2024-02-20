ml6team/fondant

commit 8c7250e07bfeda443231b545b9a8c0aec424744b

Make Fondant installable via git (#849)
This PR moves our components into the source folder so Fondant becomes
installable via git.

The reason to have them at the top level before was because we didn't
have a hub. Now that we have the hub page on the fondant.ai site, I
think the trade-off has shifted.

I will update the references in the use case repositories as well.

---------

Co-authored-by: Matthias Richter <matthias.r1092@gmail.com>

.github/workflows/prep-release.yaml
@@ -71,7 +71,6 @@ jobs:
       - name: Build a binary wheel and a source tarball
         run: |
           pip install poetry
-          ./scripts/pre-build.sh
           poetry build
 
       - name: Publish distribution 📦 to Test PyPI

pyproject.toml
@@ -21,7 +21,8 @@ maintainers = [
     "Niels Rogge <niels.rogge@ml6.eu>",
 ]
 repository = "https://github.com/ml6team/fondant"
-include = ["*.txt", "*.rst", "*.md"]
+include = ["*.txt", "*.rst", "*.md", "src/fondant/components/**/*.yaml"]
+exclude = ["src/fondant/components"]  # Exclude everything except for component specs
 classifiers = [
     "Development Status :: 2 - Pre-Alpha",
     "Intended Audience :: Developers",

scripts/post-build.sh
@@ -1,15 +1,0 @@
-#!/bin/bash
-# This script reverts the src/fondant/components directory to a symlink to the components/
-# directory.
-# It should be run after running scripts/pre-build.sh and building the fondant package
-set -e
-
-scripts_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-root_path=$(dirname "$scripts_path")
-
-pushd "$root_path"
-rm -rf src/fondant/components
-pushd src/fondant
-ln -s ../../components
-popd
-popd

scripts/pre-build.sh
@@ -1,15 +1,0 @@
-#!/bin/bash
-# This script copies the components/ directory to src/fondant/components, replacing the symlink.
-# It should be run before building the fondant package.
-# This script makes changes to the local files, which should not be committed to git. Run
-# scripts/post-build.sh to clean them up.
-set -e
-
-scripts_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-root_path=$(dirname "$scripts_path")
-
-pushd "$root_path"
-rm -rf src/fondant/components
-find components/ -type f | grep -i '\.yaml$' | xargs -I{} rsync -R "{}" src/fondant/
-
-popd

tox.ini
@@ -43,11 +43,8 @@ allowlist_externals=
     /bin/bash
     /usr/bin/bash
 commands_pre=
-    bash ./scripts/pre-build.sh
     poetry lock
     poetry install --all-extras
     poetry show
 commands=
     poetry run python -m pytest tests -vv --cov fondant --cov-report term-missing --ignore=tests/integration_tests
-commands_post=
-    bash ./scripts/post-build.sh

commit a172ab3f187417d65e08b9809a7ed01e79fa9093

Add pre- and post-build script to work around Poetry bug (#868)
The new packaging strategy introduced in #849 doesn't work correctly for
wheels due to a bug in poetry
(https://github.com/python-poetry/poetry/issues/8994), which breaks
installing both from Github and PyPi. This PR introduces a workaround.

Since the bug is related to the `src` directory, this workaround adds a
`pre-build/sh` script which unpacks the `src` directory before building,
and a `post-build.sh` script which moves the package back into the `src`
directory afterwards.

This leads to the following result:
- Installing from Github leads to an installation including all
component files
- Installing locally without running the build script leads to an
installation including all component files
- Installing from PyPi leads to an installation with only the
`fondant_component.yaml` files for each component

In the ideal case, we get the same result for all installation methods,
but this already at least leads to a working result for each method.

.github/workflows/prep-release.yaml
@@ -71,6 +71,7 @@ jobs:
       - name: Build a binary wheel and a source tarball
         run: |
           pip install poetry
+          ./scripts/pre-build.sh
           poetry build
 
       - name: Publish distribution 📦 to Test PyPI

pyproject.toml
@@ -21,8 +21,8 @@ maintainers = [
     "Niels Rogge <niels.rogge@ml6.eu>",
 ]
 repository = "https://github.com/ml6team/fondant"
-include = ["*.txt", "*.rst", "*.md", "src/fondant/components/**/*.yaml"]
-exclude = ["src/fondant/components"]  # Exclude everything except for component specs
+include = ["*.txt", "*.rst", "*.md", "fondant/components/**/*.yaml"]
+exclude = ["fondant/components"]  # Exclude everything except for component specs
 classifiers = [
     "Development Status :: 2 - Pre-Alpha",
     "Intended Audience :: Developers",
@@ -74,6 +74,9 @@ kfp = ["docker", "kfp"]
 vertex = ["docker", "kfp", "google-cloud-aiplatform"]
 sagemaker = ["sagemaker", "boto3"]
 docker = ["docker"]
+
+all = ["dask", "dask-cuda", "s3fs", "adlfs", "gcsfs", "docker", "kfp", "google-cloud-aiplatform",
+    "sagemaker", "boto3"]
 
 [tool.poetry.group.test]
 optional = true

scripts/post-build.sh
@@ -1,0 +1,14 @@
+#!/bin/bash
+# This script reverts the `scripts/pre-build.sh` script by moving the package code back into a
+# `src` directory.
+# It should be run after running scripts/pre-build.sh and building the fondant package
+set -e
+
+scripts_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+root_path=$(dirname "$scripts_path")
+
+pushd "$root_path"
+mkdir -p src/fondant
+mv fondant/* src/fondant
+rm -rf fondant
+popd

scripts/pre-build.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+# This script unpacks the src folder before building to make the poetry `include` and `exclude`
+# sections work correctly for both sdist and wheel
+# (https://github.com/python-poetry/poetry/issues/8994)
+# Building without running this script also works, but includes the full code for the components.
+# This script makes changes to the local files, which should not be committed to git. Run
+# scripts/post-build.sh to clean them up.
+set -e
+
+scripts_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+root_path=$(dirname "$scripts_path")
+
+pushd "$root_path"
+mkdir fondant
+mv src/fondant/* fondant
+rm -rf src
+popd

tox.ini
@@ -42,8 +42,10 @@ allowlist_externals=
     /bin/bash
     /usr/bin/bash
 commands_pre=
-    poetry lock
-    poetry install --all-extras
-    poetry show
+    bash ./scripts/pre-build.sh
+    poetry build
+    poetry run pip install dist/fondant-0.1.dev0-py3-none-any.whl[all]
+    poetry run pip list
+    bash ./scripts/post-build.sh
 commands=
     poetry run python -m pytest tests -vv --cov fondant --cov-report term-missing --ignore=tests/integration_tests