d9f6509fadc689a2b52c6afb663cf110d9812a69
Matthias Richter <matthias.r1092@gmail.com>
2024-03-04 17:28:36 +0100

    Addressing comments

--- src/fondant/components/retrieve_images_from_faiss_index/src/main.py
+++ src/fondant/components/retrieve_images_from_faiss_index/src/main.py
@@ -6,7 +6,7 @@ import faiss
 import fsspec
 import pandas as pd
 import torch
-from dask.distributed import Client
+from dask.distributed import Client, get_worker
 from dask_cuda import LocalCUDACluster
 from fondant.component import DaskTransformComponent
 from transformers import AutoTokenizer, CLIPTextModelWithProjection
@@ -17,18 +17,15 @@ logger = logging.getLogger(__name__)
 class RetrieveImagesFromFaissIndex(DaskTransformComponent):
     """Retrieve images from a faiss index using CLIP embeddings."""
 
-    def __init__(  # noqa PLR0913
+    def __init__(  # PLR0913
         self,
         dataset_path: str,
         faiss_index_path: str,
-        image_index_column_name: str,
         clip_model: str = "laion/CLIP-ViT-B-32-laion2B-s34B-b79K",
         num_images: int = 2,
     ):
-        self.model = CLIPTextModelWithProjection.from_pretrained(clip_model)
-        self.tokenizer = AutoTokenizer.from_pretrained(clip_model)
+        self.model_id = clip_model
         self.number_of_images = num_images
-        self.image_index_column_name = image_index_column_name
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -43,11 +40,11 @@ class RetrieveImagesFromFaissIndex(DaskTransformComponent):
 
         self.search_index = faiss.read_index("faiss_index")
 
-        self.dataset = dd.read_parquet(dataset_path)
-
-        if "url" not in self.dataset.columns:
+        dataset = dd.read_parquet(dataset_path)
+        if "url" not in dataset.columns:
             msg = "Dataset does not contain column 'url'"
             raise ValueError(msg)
+        self.image_urls = dataset["url"].compute().to_list()
 
     def setup(self) -> Client:
         if self.device == "cuda":
@@ -57,8 +54,22 @@ class RetrieveImagesFromFaissIndex(DaskTransformComponent):
         return super().setup()
 
     def embed_prompt(self, prompt: str):
-        inputs = self.tokenizer([prompt], padding=True, return_tensors="pt")
-        outputs = self.model(**inputs)
+        worker = get_worker()
+        if hasattr(worker, "model"):
+            tokenizer = worker.tokenizer
+            model = worker.model
+        else:
+            logger.info("Initializing model '%s' on worker '%s", self.model_id, worker)
+            tokenizer = AutoTokenizer.from_pretrained(self.model_id).to(self.device)
+            model = CLIPTextModelWithProjection.from_pretrained(self.model_id).to(
+                self.device,
+            )
+
+            worker.model = model
+            worker.tokenizer = tokenizer
+
+        inputs = tokenizer([prompt], padding=True, return_tensors="pt")
+        outputs = model(**inputs)
         return outputs.text_embeds.cpu().detach().numpy().astype("float64")
 
     def retrieve_from_index(
@@ -89,9 +100,7 @@ class RetrieveImagesFromFaissIndex(DaskTransformComponent):
 
             indices = self.retrieve_from_index(query, self.number_of_images)
             for i in indices:
-                url = self.dataset[self.dataset[self.image_index_column_name] == i][
-                    "url"
-                ]
+                url = self.image_urls[i]
                 row_to_add = (index, prompt, i, url) if prompt else (index, i, url)
                 results.append(row_to_add)
 
